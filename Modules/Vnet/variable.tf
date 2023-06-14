b9a7b7b093522afdaaee82cb7924a7db44b15a2d
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-06-14 13:27:26 +0000

    Updated variable.tf

--- Modules/Vnet/variable.tf
+++ Modules/Vnet/variable.tf
@@ -1,6 +1,12 @@
 variable "name"{
     type = string
 }
+variable "address_space"{
+    type = string
+
 variable "location"{
+    type = string
+
+variable "resource_group_name"{
     type = string
 }
