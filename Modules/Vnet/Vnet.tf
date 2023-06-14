268792f91523c0ae4ff47ab7e861de0cb1f5ba2d
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-06-14 13:31:37 +0000

    Updated Vnet.tf

--- Modules/Vnet/Vnet.tf
+++ Modules/Vnet/Vnet.tf
@@ -3,5 +3,5 @@ resource "azurerm_virtual_network" "my_terraform_network" {
   name                = var.name
   address_space       = var.address_space
   location            = var.location
-  resource_group_name = var.resourcegroup.rg.name
+  resource_group_name = var.resource_group_name
 }
