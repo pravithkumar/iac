cdbbaec4ed2cfdb4a3eb0f8fa0b980a94f7ad73f
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-07-17 10:32:30 +0000

    Updated backend.tf

--- Modules/storageNetworkPolicy/backend.tf
+++ Modules/storageNetworkPolicy/backend.tf
@@ -1,4 +1,18 @@
-terraform{
-    backend "azurerm" {
-    }
+
+data "azurerm_resource_group" "storagerg" {
+  name     = var.resource_group_name
+}
+
+data "azurerm_storage_account" "sa" {
+  name                = var.storageAccountName
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_storage_account_network_rules" "rules" {
+  storage_account_id = data.azurerm_storage_account.sa.id
+
+  default_action             = var.defalutaction
+  ip_rules                   = ["127.0.0.1"]
+  virtual_network_subnet_ids = var.subnetid
+  bypass                     = ["Metrics"]
 }
