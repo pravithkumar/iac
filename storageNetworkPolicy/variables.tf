42d36e8f822459ba8240de9bf75c430c9a17b15a
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-07-17 10:29:59 +0000

    Added variables.tf

---
+++ storageNetworkPolicy/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storageAccountName" {
+  type = string
+}
+
+variable "defalutaction" {
+  type = string
+}
+
+variable "subnetid" {
+  type = list(string)
+}
