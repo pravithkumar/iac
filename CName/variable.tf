57692fd9a572d64dac08731098e47b1b690e6f92
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-07-26 11:11:13 +0000

    Added variable.tf

---
+++ CName/variable.tf
@@ -0,0 +1,15 @@
+variable "name"{
+    type = string
+}
+variable "record"{
+    type = string
+
+variable "zone_name"{
+    type = string
+
+variable "resource_group_name"{
+    type = string
+}
+variable "ttl"{
+    type = string
+}
