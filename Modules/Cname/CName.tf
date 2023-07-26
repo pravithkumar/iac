66c65f8d741aefa6e1f036703ce00716c7c14392
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-07-26 11:05:12 +0000

    Updated CName.tf

--- Modules/Cname/CName.tf
+++ Modules/Cname/CName.tf
@@ -2,7 +2,6 @@ resource "azurerm_private_dns_cname_record" "cname_records" {
   name                = var.name
   record              = var.record
   resource_group_name = var.resource_group_name
-  zone_name           = azurerm_private_dns_zone.example.name
-  tags                = var.tags
+  zone_name           = var.zone_name  
   ttl                 = var.ttl
   }
