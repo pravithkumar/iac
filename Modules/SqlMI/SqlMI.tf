1eb96f066362a62bf14ceda23700a98007fb7418
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-10-10 08:01:38 +0000

    Updated SqlMI.tf

--- Modules/SqlMI/SqlMI.tf
+++ Modules/SqlMI/SqlMI.tf
@@ -26,7 +26,7 @@ resource "azurerm_mssql_managed_instance" "example" {
   storage_size_in_gb           = 32
 }
 
-resource "azurerm_mssql_managed_database" "example" {
+resource "azurerm_sql_managed_database" "example" {
   sql_managed_instance_id = azurerm_mssql_managed_instance.example.id
   name                    = "exampledatabase"
   location                = data.azurerm_resource_group.example.location
