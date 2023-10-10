381a5cdfefc16507691eeedb25cf1fc4c6311306
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-10-10 07:07:26 +0000

    Updated SqlMI.tf

--- Modules/SqlMI/SqlMI.tf
+++ Modules/SqlMI/SqlMI.tf
@@ -1,20 +1,16 @@
 data "azurerm_resource_group" "example" {
-  name     = "rg-example"
-  location = "West Europe"
+  name     = var.resource_group_name  
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "test-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+data "azurerm_virtual_network" "example" {
+  name                = "test-network" 
+  resource_group_name = var.vnet_resource_group_name
 }
 
-resource "azurerm_subnet" "example" {
+data "azurerm_subnet" "example" {
   name                 = "acctsub"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+  resource_group_name  = azurerm_virtual_network.example.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name  
 }
 
 resource "azurerm_sql_managed_instance" "example" {
