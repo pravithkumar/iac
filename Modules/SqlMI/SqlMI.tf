858ec2b3198454ff534b8c9692f97010389c79e1
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-10-11 10:05:16 +0000

    Updated SqlMI.tf

--- Modules/SqlMI/SqlMI.tf
+++ Modules/SqlMI/SqlMI.tf
@@ -3,31 +3,31 @@ data "azurerm_resource_group" "example" {
 }
 
 data "azurerm_virtual_network" "example" {
-  name                = "vnet-pega-uat-eu-001" 
+  name                = var.vnet_name
   resource_group_name = var.vnet_resource_group_name
 }
 
 data "azurerm_subnet" "example" {
-  name                 = "subnet-pega-mi-001"
+  name                 = var.subnet_name
   resource_group_name  = data.azurerm_virtual_network.example.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.example.name  
 }
 
 resource "azurerm_mssql_managed_instance" "example" {
-  name                         = "sqlmi-pega-uat-eu-001"
+  name                         = var.sqlmi_name
   resource_group_name          = data.azurerm_resource_group.example.name
   location                     = data.azurerm_resource_group.example.location
-  administrator_login          = "mradministrator"
-  administrator_login_password = "Password@123456"
-  license_type                 = "BasePrice"
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  license_type                 = var.license_type
   subnet_id                    = data.azurerm_subnet.example.id
-  sku_name                     = "GP_Gen5"
-  vcores                       = 4
-  storage_size_in_gb           = 32
+  sku_name                     = var.sku_name
+  vcores                       = var.vcores
+  storage_size_in_gb           = var.storage_size_in_gb
 }
 
 resource "azurerm_mssql_managed_database" "example" {
   managed_instance_id = azurerm_mssql_managed_instance.example.id
-  name                    = "sqldb-pega-uat-eu-001"
+  name                    = var.dbname
   
 }
