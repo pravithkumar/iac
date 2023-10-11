aec80c56f09ac0d06e0a6854d6762025884e5e95
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2023-10-11 10:05:53 +0000

    Updated variables.tf

--- CreateSqlMI/variables.tf
+++ CreateSqlMI/variables.tf
@@ -1,6 +1,47 @@
 variable "resource_group_name" {
   type = string
 }
+
 variable "vnet_resource_group_name" {
-  type = string  
+  type = string
 }
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "sqlmi_name" {
+  type = string
+}
+
+variable "administrator_login" {
+  type = string
+}
+
+variable "administrator_login_password" {
+  type = string
+}
+
+variable "license_type" {
+  type = string
+}
+
+variable "sku_name" {
+  type = string
+}
+
+variable "vcores" {
+  type = string
+}
+variable "storage_size_in_gb " {
+  type = string
+}
+
+variable "dbname" {
+  type = string
+}
+
