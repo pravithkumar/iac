dfb018aa5667edc811565ca41403b4fc438a2f84
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-14 09:21:52 +0000

    Updated main.tf

--- Servicebus/main.tf
+++ Servicebus/main.tf
@@ -7,6 +7,6 @@ module "servicebus" {
   resource_group_name = var.resource_group_name
   location            = var.location
   servicebus_name     = var.servicebus_name
-  sku                 = var.servicebus_sku
+  sku                 = var.sku
   private_endpoints   = var.private_endpoints
 }
