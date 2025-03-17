bfcffb9ba72d07a591086f655a25cd2a85460692
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-17 08:51:51 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -1,96 +1,29 @@
 # main.tf
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+resource "azurerm_function_app" "function_app" {
+  source                     = "./modules/function_app"
+  function_app_name          = var.function_app_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  app_service_plan_id        = var.app_service_plan_id
+  storage_account_name       = var.storage_account_name
+  storage_account_resource_group_name = var.storage_account_resource_group_name
+  app_insights_name          = var.app_insights_name
+  app_insights_resource_group_name = var.app_insights_resource_group_name
+  runtime                    = var.runtime
+  runtime_version            = var.runtime_version
+  https_only                 = var.https_only
+  always_on                  = var.always_on
+  tags                       = var.tags
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-}
-
-# Resource Group
-resource "azurerm_resource_group" "function_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# App Service Plan
-resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.function_rg.location
-  resource_group_name = azurerm_resource_group.function_rg.name
-  kind                = var.app_service_plan_kind
-  reserved            = var.app_service_plan_reserved
-  sku {
-    tier = var.app_service_plan_tier
-    size = var.app_service_plan_size
-  }
-}
-
-# Storage Account
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "${var.storage_account_name}${random_integer.result}"
-  resource_group_name      = azurerm_resource_group.function_rg.name
-  location                 = azurerm_resource_group.function_rg.location
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.storage_account_replication_type
-}
-
-resource "random_integer" "random_integer" {
-  min = 10000
-  max = 99999
-}
-
-# Application Insights
-resource "azurerm_application_insights" "appinsights" {
-  name                = var.application_insights_name
-  location            = azurerm_resource_group.function_rg.location
-  resource_group_name = azurerm_resource_group.function_rg.name
-  application_type    = var.application_insights_type
-}
-
-# Function App
-resource "azurerm_function_app" "functionapp" {
-  name                       = var.function_app_name
-  location                   = azurerm_resource_group.function_rg.location
-  resource_group_name        = azurerm_resource_group.function_rg.name
-  app_service_plan_id        = azurerm_app_service_plan.appserviceplan.id
-  storage_account_name       = azurerm_storage_account.storageaccount.name
-  storage_account_access_key = azurerm_storage_account.storageaccount.primary_access_key
-  version                    = var.function_app_version
-  https_only                 = var.function_app_https_only
-
-  site_config {
-    always_on = var.function_app_always_on
-    application_stack {
-      python_version = var.python_version
-    }
-    cors {
-      allowed_origins = var.allowed_origins
-    }
-  }
-
-  app_settings = {
-    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appinsights.instrumentation_key
-    FUNCTIONS_WORKER_RUNTIME        = var.function_worker_runtime
-    AzureWebJobsStorage             = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storageaccount.name};AccountKey=${azurerm_storage_account.storageaccount.primary_access_key};EndpointSuffix=core.windows.net"
-  }
-    identity {
-      type = var.function_app_identity_type
-  }
-}
-
-output "function_app_default_hostname" {
-  value = azurerm_function_app.functionapp.default_hostname
-}
-
-output "principal_id" {
-  value = azurerm_function_app.functionapp.identity[0].principal_id
+resource "azurerm_private_endpoint" "function_app_pe" {
+  source                     = "./modules/private_endpoint"
+  private_endpoint_name      = "${var.function_app_name}-pe"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  subnet_id                  = var.private_endpoint_subnet_id
+  function_app_id            = azurerm_function_app.function_app.id
+  private_dns_zone_id        = var.private_dns_zone_id
+  tags                       = var.tags
 }
