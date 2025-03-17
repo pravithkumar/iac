37f7f71ebde16959c61bb6b08edf774527442016
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-17 09:00:00 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
-resource "azurerm_function_app" "function_app" {
-  source                     = "./modules/function_app"
+module "function_app_and_pe" {
+  source                     = "./modules/function_app_and_pe"
   function_app_name          = var.function_app_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
@@ -15,15 +15,6 @@ resource "azurerm_function_app" "function_app" {
   https_only                 = var.https_only
   always_on                  = var.always_on
   tags                       = var.tags
-}
-
-resource "azurerm_private_endpoint" "function_app_pe" {
-  source                     = "./modules/private_endpoint"
-  private_endpoint_name      = "${var.function_app_name}-pe"
-  location                   = var.location
-  resource_group_name        = var.resource_group_name
-  subnet_id                  = var.private_endpoint_subnet_id
-  function_app_id            = azurerm_function_app.function_app.id
+  private_endpoint_subnet_id = var.private_endpoint_subnet_id
   private_dns_zone_id        = var.private_dns_zone_id
-  tags                       = var.tags
 }
