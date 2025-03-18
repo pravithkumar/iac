dd4b282966fefa598ff886b7b159b862d34b2162
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-18 07:41:46 +0000

    Updated variables.tf

--- Modules/AzureFunctionApp/variables.tf
+++ Modules/AzureFunctionApp/variables.tf
@@ -12,10 +12,9 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "app_service_plan_id" {
+variable "app_service_plan_name" { 
   type = string
 }
-
 variable "storage_account_name" {
   type = string
 }
