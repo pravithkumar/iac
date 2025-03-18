921fdb144805ef510cf6d6a96dee0df74eb61112
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-18 07:44:25 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -5,7 +5,6 @@ module "function_app_and_pe" {
   function_app_name                     = var.function_app_name
   location                              = var.location
   resource_group_name                   = var.resource_group_name
-  app_service_plan_id                   = var.app_service_plan_id
   storage_account_name                  = var.storage_account_name
   storage_account_resource_group_name   = var.storage_account_resource_group_name
   app_insights_name                     = var.app_insights_name
