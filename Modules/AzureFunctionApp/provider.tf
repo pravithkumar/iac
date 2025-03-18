889061ab2e316ab32e31773e064e355618a76959
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-18 06:03:33 +0000

    Updated provider.tf

--- Modules/AzureFunctionApp/provider.tf
+++ Modules/AzureFunctionApp/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.0" # Or the latest version (e.g., "~> 3.70")
+      version = "3.70.0" # Explicit version
     }
   }
 }
