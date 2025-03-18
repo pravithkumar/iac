9eb5fe8176d0b0f12fe686537442e2bac13430b6
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-18 05:57:16 +0000

    Updated variables.tf

--- CreateFunctionApp/variables.tf
+++ CreateFunctionApp/variables.tf
@@ -47,3 +47,15 @@ variable "https_only" {
 variable "always_on" {
   type = bool
 }
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "private_endpoint_subnet_id" {
+  type = string
+}
+
+variable "private_dns_zone_id" {
+  type = string
+}
