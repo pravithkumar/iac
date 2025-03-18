e43c53239c5bfe801978712f54a878724b8c19f9
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-18 05:52:13 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -1,20 +1,20 @@
 # main.tf
 
 module "function_app_and_pe" {
-  source                     = "../Modules/AzureFunctionApp"
-  function_app_name          = var.function_app_name
-  location                   = var.location
-  resource_group_name        = var.resource_group_name
-  app_service_plan_id        = var.app_service_plan_id
-  storage_account_name       = var.storage_account_name
-  storage_account_resource_group_name = var.storage_account_resource_group_name
-  app_insights_name          = var.app_insights_name
-  app_insights_resource_group_name = var.app_insights_resource_group_name
-  runtime                    = var.runtime
-  runtime_version            = var.runtime_version
-  https_only                 = var.https_only
-  always_on                  = var.always_on
-  tags                       = var.tags
-  private_endpoint_subnet_id = var.private_endpoint_subnet_id
-  private_dns_zone_id        = var.private_dns_zone_id
+  source                                = "../Modules/AzureFunctionApp"
+  function_app_name                     = var.function_app_name
+  location                              = var.location
+  resource_group_name                   = var.resource_group_name
+  app_service_plan_id                   = var.app_service_plan_id
+  storage_account_name                  = var.storage_account_name
+  storage_account_resource_group_name   = var.storage_account_resource_group_name
+  app_insights_name                     = var.app_insights_name
+  app_insights_resource_group_name      = var.app_insights_resource_group_name
+  runtime                               = var.runtime
+  runtime_version                       = var.runtime_version
+  https_only                            = var.https_only
+  always_on                             = var.always_on
+  tags                                  = var.tags
+  private_endpoint_subnet_id            = var.private_endpoint_subnet_id
+  private_dns_zone_id                   = var.private_dns_zone_id
 }
