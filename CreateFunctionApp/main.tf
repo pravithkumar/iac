7c28bcb2731b7579a580fbda1f3c6bcf30e5fcbf
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-20 15:03:53 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -18,3 +18,14 @@ module "function_app_and_pe" {
   private_endpoint_subnet_id            = var.private_endpoint_subnet_id
   private_dns_zone_id                   = var.private_dns_zone_id
 }
+
+resource "azurerm_service_plan" "asp" {
+  provider            = azurerm.aiml-nprod-001
+  name                = var.service_plan_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type
+  sku_name            = var.aspsku_name
+  worker_count        = var.worker_count
+  tags                = var.tags
+}
