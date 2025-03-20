bd4b86b57e4ad04767b39fba1520cf2a7bb7e421
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-20 14:41:28 +0000

    Deleted CName

--- Modules/CName/CName.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_private_dns_cname_record" "cname_records" {
-  name                = var.name
-  record              = var.record
-  resource_group_name = var.resource_group_name
-  zone_name           = var.zone_name  
-  ttl                 = var.ttl
-  }
