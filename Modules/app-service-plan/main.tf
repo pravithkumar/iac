8e542a0287748317a42d8e257f6962c50ce72a30
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 06:02:58 +0000

    Updated main.tf

--- Modules/app-service-plan/main.tf
+++ Modules/app-service-plan/main.tf
@@ -1,8 +1,12 @@
+data "azurerm_resource_group" "rg" {
+  name = var.asp_resource_group_name
+}
+
 resource "azurerm_service_plan" "asp" {
   provider = azurerm.aiml-nprod-001
   name                = var.service_plan_name
   resource_group_name = var.resource_group_name
-  location            = var.location
+  location            = data.azurerm_resource_group.rg.location
   os_type             = var.os_type
   sku_name = var.aspsku_name
   worker_count = var.worker_count
