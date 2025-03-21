e6a9c71f9fcdfde2dc0682625d118bffe8e6c259
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 10:46:01 +0000

    Added variables.tf

---
+++ Modules/storageaccount/variables.tf
@@ -0,0 +1,26 @@
+// modules/storage/variables.tf
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+
+}
