6008c8c412f9857989df3d12b93d6f08801d2af8
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 08:29:12 +0000

    Updated main.tf

--- Modules/private-endpoint/main.tf
+++ Modules/private-endpoint/main.tf
@@ -1,38 +1,20 @@
-#vnet for aks
-data "azurerm_virtual_network" "existing_vnet" {
-  name                = var.existing_vnet_name #vnet-xl-nonprod-eastus-001
-  #resource_group_name = var.resource_group_name
-  resource_group_name = var.rg_for_vnet 
-}
+# Modules/PrivateEndpoint/private_endpoint.tf
 
-resource "azurerm_private_endpoint" "acr_private_endpoint" {
-  name                = "${var.acr_name}-private-endpoint"
-  location            =  var.location 
+resource "azurerm_private_endpoint" "pe" {
+  name                = var.private_endpoint_name
+  location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id  
+  subnet_id           = var.subnet_id
 
   private_service_connection {
-    name                           = "${var.acr_name}-privatelink"
-    private_connection_resource_id = var.private_connection_resource_id   #module.acr-mode.acr_id.id
-    is_manual_connection           = false
-    subresource_names              = ["registry"]  # To connect to the ACR registry service
+    name                           = var.private_service_connection_name
+    private_connection_resource_id = var.private_connection_resource_id
+    subresource_names              = var.subresource_names
+    is_manual_connection           = var.is_manual_connection
   }
-}
-resource "azurerm_private_dns_zone" "acr_private_dns_zone" {
-  name                = var.aks_acr_private_dns_name 
-  resource_group_name = var.resource_group_name
-}
 
-resource "azurerm_private_dns_zone_virtual_network_link" "acr_vnet_link" {
-  name                  = "${var.acr_name}-dns-link"
-  resource_group_name = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.acr_private_dns_zone.name
-  virtual_network_id    =  data.azurerm_virtual_network.existing_vnet.id
-}
-resource "azurerm_private_dns_a_record" "acr_private_dns_record" {
-  name                = var.acr_name # Use the ACR name for the DNS record
-  zone_name           = azurerm_private_dns_zone.acr_private_dns_zone.name
-  resource_group_name = var.resource_group_name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.acr_private_endpoint.private_service_connection[0].private_ip_address]  
+  private_dns_zone_group {
+    name                 = var.private_dns_zone_group_name
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
 }
