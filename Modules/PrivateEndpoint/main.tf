a97b8a1c282895e34baab4511b7905713780c864
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 09:50:35 +0000

    Updated main.tf

--- Modules/PrivateEndpoint/main.tf
+++ Modules/PrivateEndpoint/main.tf
@@ -1,17 +1,5 @@
 # Modules/PrivateEndpoint/private_endpoint.tf
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.private_endpoint_name}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_private_endpoint" "pe" {
   name                = var.private_endpoint_name
   location            = var.location
