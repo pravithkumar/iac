cd19e395f60a07ea316e60b87898de04a1ceaab0
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 08:31:49 +0000

    Updated functionapp.tf

--- Modules/AzureFunctionApp/functionapp.tf
+++ Modules/AzureFunctionApp/functionapp.tf
@@ -1,6 +1,3 @@
-# Modules/AzureFunctionApp/functionapp.tf
-
-# Data sources for existing resources
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
@@ -11,8 +8,8 @@ data "azurerm_storage_account" "sa" {
 }
 
 data "azurerm_service_plan" "asp" {
-  name                = var.app_service_plan_name # Use name
-  resource_group_name = var.resource_group_name # Use resource group
+  name                = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
 }
 
 data "azurerm_application_insights" "ai" {
@@ -24,31 +21,13 @@ resource "azurerm_linux_function_app" "fa" {
   name                       = var.function_app_name
   location                   = var.location
   resource_group_name        = data.azurerm_resource_group.rg.name
-  service_plan_id            = data.azurerm_service_plan.asp.id  
+  service_plan_id            = data.azurerm_service_plan.asp.id
   storage_account_name       = data.azurerm_storage_account.sa.name
   storage_account_access_key = data.azurerm_storage_account.sa.primary_access_key
-  https_only = var.https_only  
-  tags = var.tags
-   site_config {  
+  https_only                 = var.https_only
+  tags                       = var.tags
+
+  site_config {
     always_on = var.always_on
-  }
-}
-
-resource "azurerm_private_endpoint" "pe" {
-  name                = "${var.function_app_name}-pe"
-  location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  subnet_id           = var.private_endpoint_subnet_id
-
-  private_service_connection {
-    name                           = "${var.function_app_name}-psc"
-    private_connection_resource_id = azurerm_linux_function_app.fa.id
-    subresource_names              = ["sites"]
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group"
-    private_dns_zone_ids = [var.private_dns_zone_id]
   }
 }
