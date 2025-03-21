c61d88901c2ef9eb572516e1a9baa6510d78fb92
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-21 05:18:04 +0000

    Updated functionapp.tf

--- Modules/AzureFunctionApp/functionapp.tf
+++ Modules/AzureFunctionApp/functionapp.tf
@@ -20,11 +20,10 @@ data "azurerm_application_insights" "ai" {
   resource_group_name = var.app_insights_resource_group_name
 }
 
-resource "azurerm_function_app" "fa" {
+resource "azurerm_linux_function_app" "fa" {
   name                       = var.function_app_name
   location                   = data.azurerm_resource_group.rg.location
-  resource_group_name        = data.azurerm_resource_group.rg.name
-  os_type                         = "linux" 
+  resource_group_name        = data.azurerm_resource_group.rg.name  
   app_service_plan_id        = data.azurerm_service_plan.asp.id
   storage_account_name       = data.azurerm_storage_account.sa.name
   storage_account_access_key = data.azurerm_storage_account.sa.primary_access_key
