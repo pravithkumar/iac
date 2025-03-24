f67f3bae995d886a1074c4bb1b25c6fbe3fdef1e
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-24 12:28:02 +0000

    Updated output.tf

--- Modules/PrivateEndpoint/output.tf
+++ Modules/PrivateEndpoint/output.tf
@@ -1,7 +1,7 @@
 output "private_endpoint_name" {
-  value = azurerm_private_endpoint.pe.name  # Corrected reference
+  value = azurerm_private_endpoint.pe.name 
 }
 
 output "private_dns_name" {
-  value = azurerm_private_endpoint.pe.private_dns_zone_group[0].name  # Corrected reference
+  value = azurerm_private_endpoint.pe.private_dns_zone_group[0].name  
 }
