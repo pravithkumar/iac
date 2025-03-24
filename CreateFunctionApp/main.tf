e9f245e96dddb8f6595add7c4179ab0bc70dac45
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-24 11:59:49 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -3,21 +3,21 @@ provider "azurerm" {
 }
 
 module "azurerm_linux_function_app" {
-  source                                = "../Modules/AzureFunctionApp"
-  function_app_name                     = var.function_app_name
-  location                              = var.location
-  resource_group_name                   = var.resource_group_name
-  storage_account_name                  = var.storage_account_name
-  storage_account_resource_group_name   = var.storage_account_resource_group_name
-  app_insights_name                     = var.app_insights_name
-  app_insights_resource_group_name      = var.app_insights_resource_group_name
-  app_service_plan_name                 = var.asp_service_plan_name
-  runtime                               = var.runtime
-  runtime_version                       = var.runtime_version
-  https_only                            = var.https_only
-  always_on                             = var.always_on
-  tags                                  = var.tags
-  depends_on                            = [azurerm_service_plan.asp]
+  source                              = "../Modules/AzureFunctionApp"
+  function_app_name                   = var.function_app_name
+  location                            = var.location
+  resource_group_name                 = var.resource_group_name
+  storage_account_name                = var.storage_account_name
+  storage_account_resource_group_name = var.storage_account_resource_group_name
+  app_insights_name                   = var.app_insights_name
+  app_insights_resource_group_name    = var.app_insights_resource_group_name
+  app_service_plan_name               = var.asp_service_plan_name
+  runtime                             = var.runtime
+  runtime_version                     = var.runtime_version
+  https_only                          = var.https_only
+  always_on                           = var.always_on
+  tags                                = var.tags
+  depends_on                          = [azurerm_service_plan.asp]
 }
 
 resource "azurerm_service_plan" "asp" {
@@ -37,14 +37,13 @@ module "private_endpoint_function_app" {
   resource_group_name             = var.resource_group_name
   subnet_id                       = var.private_endpoint_subnet_id
   private_service_connection_name = "${var.function_app_name}-psc"
-  private_connection_resource_id  = module.azurerm_linux_function_app.function_app_id  
+  private_connection_resource_id  = module.azurerm_linux_function_app.function_app_id
   subresource_names               = ["sites"]
   is_manual_connection            = false
   private_dns_zone_group_name     = "private-dns-zone-group"
   private_dns_zone_ids            = [var.private_dns_zone_id]
   depends_on                      = [module.azurerm_linux_function_app]
 }
-
 
 module "storage" {
   source                          = "../Modules/Storageaccount"
@@ -55,7 +54,6 @@ module "storage" {
   account_replication_type        = var.account_replication_type
   tags                            = var.tags
 }
-
 
 module "servicebus" {
   source                = "../Modules/servicebus"
