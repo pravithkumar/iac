e695667cbffc672dcd5c676ec5d98c3248e3b0a9
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-24 09:14:19 +0000

    Updated servicebus.tf

--- Modules/servicebus/servicebus.tf
+++ Modules/servicebus/servicebus.tf
@@ -1,25 +1,3 @@
-resource "azurerm_private_endpoint" "servicebus_pe" {
-  for_each            = var.sku == "Premium" && length(var.private_endpoints) > 0 ? { for pe in var.private_endpoints : pe.name => pe } : {}
-  name                = each.value.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = each.value.subnet_id
-
-  private_service_connection {
-    name                           = "servicebus-private-connection-${each.key}"
-    private_connection_resource_id = azurerm_servicebus_namespace.servicebus.id
-    subresource_names              = each.value.subresource_names
-    is_manual_connection           = false
-  }
-
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group-${each.key}"
-    private_dns_zone_ids = each.value.private_dns_zone_ids
-  }
-
-  depends_on = [azurerm_servicebus_namespace.servicebus]
-}
-
 resource "azurerm_servicebus_namespace" "servicebus" {
   name                = var.servicebus_name
   location            = var.location
