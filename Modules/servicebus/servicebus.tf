bbb2373bc6136f05038e811a832ade1794dc4580
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 11:36:12 +0000

    Updated servicebus.tf

--- Modules/servicebus/servicebus.tf
+++ Modules/servicebus/servicebus.tf
@@ -3,7 +3,7 @@ resource "azurerm_servicebus_namespace" "servicebus" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-
+  public_network_access_enabled = false
   capacity                     = var.sku == "Premium" ? var.servicebus_capacity : null
   premium_messaging_partitions = var.sku == "Premium" ? var.premium_messaging_partitions : null
 }
