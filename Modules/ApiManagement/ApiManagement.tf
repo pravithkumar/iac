a77c703cdbddc926c9ea2d10fffd1a9756031dcc
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 10:24:10 +0000

    Updated ApiManagement.tf

--- Modules/ApiManagement/ApiManagement.tf
+++ Modules/ApiManagement/ApiManagement.tf
@@ -1,4 +1,4 @@
-resource "azurerm_api_management" "main" {
+resource "azurerm_api_management" "api" {
   name                = var.api_management_name
   location            = var.location
   resource_group_name = var.resource_group_name
