6bd71d087a4aee9e5ef3ba7504aa30102e5d28ab
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 05:11:51 +0000

    Updated output.tf

--- Modules/PrivateEndpoint/output.tf
+++ Modules/PrivateEndpoint/output.tf
@@ -3,7 +3,7 @@ output "private_endpoint_id" {
 }
 
 output "private_service_connection_id" {
-  value = azurerm_private_endpoint.pe.private_service_connection[0].id
+  value = azurerm_private_endpoint.pe.private_service_connection[0].private_connection_resource_id
 }
 
 output "private_dns_zone_group_id" {
