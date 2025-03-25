007fae42197805e3e0e06b9bba54f432695eefb6
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 11:18:36 +0000

    Updated variables.tf

--- CreateApiManagement/variables.tf
+++ CreateApiManagement/variables.tf
@@ -1,7 +1,3 @@
-variable "function_app_name" {
-  type = string
-}
-
 variable "location" {
   type = string
 }
@@ -10,85 +6,6 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "storage_account_name" {
-  type = string
-}
-
-variable "storage_account_resource_group_name" {
-  type = string
-}
-
-variable "app_insights_name" {
-  type = string
-}
-
-variable "app_insights_resource_group_name" {
-  type = string
-}
-
-variable "runtime" {
-  type = string
-}
-
-variable "runtime_version" {
-  type = string
-}
-
-variable "https_only" {
-  type = bool
-}
-
-variable "always_on" {
-  type = bool
-}
-
-variable "tags" {
-  type = map(string)
-}
-
-variable "asp_resource_group_name" {
-  type = string
-}
-
-variable "asp_service_plan_name" {
-  type = string
-}
-
-variable "os_type" {
-  type = string
-}
-
-variable "aspsku_name" {
-  type = string
-}
-
-variable "worker_count" {
-  type = number
-}
-
-variable "private_endpoint_subnet_id" {
-  type = string
-}
-
-variable "private_dns_zone_id" {
-  type = string
-}
-
-variable "account_tier" {
-  type = string
-}
-
-variable "account_replication_type" {
-  type = string
-}
-
-variable "servicebus_name" {
-  type = string
-}
-
-variable "sku" {
-  type = string
-}
 
 variable "private_endpoints" {
   type = list(object({
@@ -99,33 +16,6 @@ variable "private_endpoints" {
   }))
 }
 
-variable "queue_names" {
-  type = list(string)
-}
-
-variable "topic_names" {
-  type = list(string)
-}
-
-variable "servicebus_capacity" {
-  type = number
-}
-
-variable "premium_messaging_partitions" {
-  type = number
-}
-
-variable "queue_max_size" {
-  type = number
-}
-
-variable "topic_max_size" {
-  type = number
-}
-
-variable "subscription_max_delivery_count" {
-  type = number
-}
 
 variable "api_management_name" {
   type = string
