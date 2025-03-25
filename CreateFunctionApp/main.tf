c2db30c3a768a7f734fca8cbc1f3d8491271cfc1
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 04:36:34 +0000

    Updated main.tf

--- CreateFunctionApp/main.tf
+++ CreateFunctionApp/main.tf
@@ -1,23 +1,24 @@
+// main.tf (Corrected)
 provider "azurerm" {
   features {}
 }
 
 module "azurerm_linux_function_app" {
-  source                              = "../Modules/AzureFunctionApp"
-  function_app_name                   = var.function_app_name
-  location                            = var.location
-  resource_group_name                 = var.resource_group_name
-  storage_account_name                = var.storage_account_name
+  source                               = "../Modules/AzureFunctionApp"
+  function_app_name                      = var.function_app_name
+  location                               = var.location
+  resource_group_name                    = var.resource_group_name
+  storage_account_name                   = var.storage_account_name
   storage_account_resource_group_name = var.storage_account_resource_group_name
-  app_insights_name                   = var.app_insights_name
-  app_insights_resource_group_name    = var.app_insights_resource_group_name
-  app_service_plan_name               = var.asp_service_plan_name
-  runtime                             = var.runtime
-  runtime_version                     = var.runtime_version
-  https_only                          = var.https_only
-  always_on                           = var.always_on
-  tags                                = var.tags
-  depends_on                          = [azurerm_service_plan.asp]
+  app_insights_name                      = var.app_insights_name
+  app_insights_resource_group_name     = var.app_insights_resource_group_name
+  app_service_plan_name                  = var.asp_service_plan_name
+  runtime                                = var.runtime
+  runtime_version                        = var.runtime_version
+  https_only                             = var.https_only
+  always_on                              = var.always_on
+  tags                                   = var.tags
+  depends_on                              = [azurerm_service_plan.asp]
 }
 
 resource "azurerm_service_plan" "asp" {
@@ -31,49 +32,49 @@ resource "azurerm_service_plan" "asp" {
 }
 
 module "private_endpoint_function_app" {
-  source                          = "../Modules/PrivateEndpoint"
-  private_endpoint_name           = "${var.function_app_name}-pe"
-  location                        = var.location
-  resource_group_name             = var.resource_group_name
-  subnet_id                       = var.private_endpoint_subnet_id
-  private_service_connection_name = "${var.function_app_name}-psc"
-  private_connection_resource_id  = module.azurerm_linux_function_app.function_app_id
-  subresource_names               = ["sites"]
-  is_manual_connection            = false
-  private_dns_zone_group_name     = "private-dns-zone-group"
-  private_dns_zone_ids            = [var.private_dns_zone_id]
-  depends_on                      = [module.azurerm_linux_function_app]
+  source                               = "../Modules/PrivateEndpoint"
+  private_endpoint_name                  = "${var.function_app_name}-pe"
+  location                               = var.location
+  resource_group_name                    = var.resource_group_name
+  subnet_id                              = var.private_endpoint_subnet_id
+  private_service_connection_name        = "${var.function_app_name}-psc"
+  private_connection_resource_id         = module.azurerm_linux_function_app.function_app_id
+  subresource_names                      = ["sites"]
+  is_manual_connection                   = false
+  private_dns_zone_group_name            = "private-dns-zone-group"
+  private_dns_zone_ids                   = [var.private_dns_zone_id]
+  depends_on                              = [module.azurerm_linux_function_app]
 }
 
 module "storage" {
-  source                          = "../Modules/Storageaccount"
-  resource_group_name             = var.resource_group_name
-  storage_account_name            = var.storage_account_name
-  location                        = var.location
-  account_tier                    = var.account_tier
-  account_replication_type        = var.account_replication_type
-  tags                            = var.tags
+  source                     = "../Modules/Storageaccount"
+  resource_group_name        = var.resource_group_name
+  storage_account_name       = var.storage_account_name
+  location                   = var.location
+  account_tier               = var.account_tier
+  account_replication_type = var.account_replication_type
+  tags                       = var.tags
 }
 
 module "servicebus" {
-  source                = "../Modules/servicebus"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  servicebus_name       = var.servicebus_name
-  sku                   = var.sku
+  source              = "../Modules/servicebus"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  servicebus_name     = var.servicebus_name
+  sku                 = var.sku
 }
 
 module "private_endpoint_servicebus" {
-  source                          = "../Modules/PrivateEndpoint"
-  private_endpoint_name           = "${var.servicebus_name}-pe"
-  location                        = var.location
-  resource_group_name             = var.resource_group_name
-  subnet_id                       = var.private_endpoint_subnet_id
-  private_service_connection_name = "${var.servicebus_name}-psc"
-  private_connection_resource_id  = module.servicebus.servicebus_id
-  subresource_names               = ["namespace"]
-  is_manual_connection            = false
-  private_dns_zone_group_name     = "private-dns-zone-group"
-  private_dns_zone_ids            = [var.private_dns_zone_id]
-  depends_on                      = [module.servicebus]
+  source                               = "../Modules/PrivateEndpoint"
+  private_endpoint_name                  = "${var.servicebus_name}-pe"
+  location                               = var.location
+  resource_group_name                    = var.resource_group_name
+  subnet_id                              = var.private_endpoint_subnet_id
+  private_service_connection_name        = "${var.servicebus_name}-psc"
+  private_connection_resource_id         = module.servicebus.servicebus_id
+  subresource_names                      = ["namespace"]
+  is_manual_connection                   = false
+  private_dns_zone_group_name            = "private-dns-zone-group"
+  private_dns_zone_ids                   = [var.private_dns_zone_id]
+  depends_on                              = [module.servicebus]
 }
