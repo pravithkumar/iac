060c2423a5a464b79c7319021a716f7f5031ec21
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 11:57:19 +0000

    Updated main.tf

--- CreateApiManagement/main.tf
+++ CreateApiManagement/main.tf
@@ -17,15 +17,15 @@ module "api_management" {
 
 module "private_endpoint_api_management" {
   source                          = "../Modules/PrivateEndpoint"
-  private_endpoint_name           = var.private_endpoints[2].name
+  private_endpoint_name           = var.private_endpoints[0].name
   location                        = var.location
   resource_group_name             = var.resource_group_name
-  subnet_id                       = var.private_endpoints[2].subnet_id
+  subnet_id                       = var.private_endpoints[0].subnet_id
   private_service_connection_name = "${var.api_management_name}-psc"
   private_connection_resource_id  = module.api_management.id
-  subresource_names               = var.private_endpoints[2].subresource_names
+  subresource_names               = var.private_endpoints[0].subresource_names
   is_manual_connection            = false
   private_dns_zone_group_name     = "private-dns-zone-group"
-  private_dns_zone_ids            = var.private_endpoints[2].private_dns_zone_ids
+  private_dns_zone_ids            = var.private_endpoints[0].private_dns_zone_ids
   depends_on                      = [module.api_management]
 }
