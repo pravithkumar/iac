56511d611b64b8120f0d2ee53413fefbd2e25c96
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 06:45:47 +0000

    Updated logicapp.tf

--- Modules/LogicApp/logicapp.tf
+++ Modules/LogicApp/logicapp.tf
@@ -3,8 +3,16 @@ resource "azurerm_logic_app_standard" "logic_app" {
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
-  workflow {
-    definition = var.workflow_definition
+  storage_account_name = var.storage_account_name
+  storage_account_access_key = var.storage_account_access_key
+
+  site_config {
+    app_settings = {
+      "WORKFLOWS_SUBSCRIPTION_ID" = var.subscription_id
+      "WORKFLOWS_RESOURCE_GROUP_NAME" = var.resource_group_name
+      "WORKFLOWS_STORAGE_ACCOUNT_NAME" = var.storage_account_name
+    }
   }
+
   tags = var.tags
 }
