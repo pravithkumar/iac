3734fb91d960ecdc8684231d568c3f6e4a2d88c6
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-25 06:20:50 +0000

    Added variables.tf

---
+++ Modules/ApiManagement/variables.tf
@@ -0,0 +1,27 @@
+variable "api_management_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "publisher_name" {
+  type = string
+}
+
+variable "publisher_email" {
+  type = string
+}
+
+variable "sku_name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
