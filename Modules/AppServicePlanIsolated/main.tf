8d5b1235133382cf9c81bf7763feb04f72f04e6b
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-26 15:06:50 +0000

    Updated main.tf

--- Modules/AppServicePlanIsolated/main.tf
+++ Modules/AppServicePlanIsolated/main.tf
@@ -1,6 +1,6 @@
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_app_service_plan" "app_service_plan" {
   name                = var.app_service_plan_name
-  location            = data.azurerm_virtual_network.existing.location
+  location            = data.azurerm_virtual_network.existing_vnet.location
   resource_group_name = var.existing_vnet_resource_group
   kind                = "App"
   reserved            = true
@@ -8,5 +8,5 @@ resource "azurerm_app_service_plan" "asp" {
     tier = var.app_service_plan_sku_tier
     size = var.app_service_plan_sku_size
   }
-  app_service_environment_id = azurerm_app_service_environment_v3.asp.id
+  app_service_environment_id = azurerm_app_service_environment_v3.ase.id
 }
