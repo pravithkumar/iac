ff465fe3efc12a929f7c0f65f755a179c0460495
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-26 15:07:16 +0000

    Updated AppServiceEnvironment.tf

--- Modules/AppServiceEnvironment/AppServiceEnvironment.tf
+++ Modules/AppServiceEnvironment/AppServiceEnvironment.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_environment_v3" "example" {
+resource "azurerm_app_service_environment_v3" "ase" {
   name                          = var.ase_name
   resource_group_name           = var.resource_group_name
   subnet_id                     = var.subnet_id
