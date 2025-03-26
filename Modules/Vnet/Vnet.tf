cf71fc5bd1e3379b334f94185dcd75d39fb66e9c
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-26 05:04:00 +0000

    Deleted Vnet

--- Modules/Vnet/Vnet.tf
+++
@@ -1,7 +0,0 @@
-# Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = var.name
-  address_space       = var.address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
