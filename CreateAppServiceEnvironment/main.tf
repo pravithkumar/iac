d5aa3f654178d112e9f29555724413c8d207d970
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-26 11:09:57 +0000

    Updated main.tf

--- CreateAppServiceEnvironment/main.tf
+++ CreateAppServiceEnvironment/main.tf
@@ -1,20 +1,12 @@
-provider "azurerm" {
-  features {}
-}
-
 module "app_service_environment" {
   source                          = "../Modules/AppServiceEnvironment"
   ase_name                        = var.ase_name
-  location                        = var.location
   resource_group_name             = var.resource_group_name
   subnet_id                       = var.subnet_id
   internal_load_balancing_mode    = var.internal_load_balancing_mode
-  virtual_ip_type                 = var.virtual_ip_type
-  physical_hardware_isolation     = var.physical_hardware_isolation
-  zone_redundancy                 = var.zone_redundancy
-  dns_settings                    = var.dns_settings
-  inbound_ip_address              = var.inbound_ip_address
-  cluster_settings                = var.cluster_settings
+  dns_suffix                      = var.dns_suffix
+  disable_tls1_0                  = var.disable_tls1_0
+  internal_encryption             = var.internal_encryption
+  frontend_ssl_cipher_suite_order = var.frontend_ssl_cipher_suite_order
   tags                            = var.tags
 }
-
