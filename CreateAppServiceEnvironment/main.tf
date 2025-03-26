269afccab5ecd44a9f831f7cc3a111fc15b37120
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-26 11:19:59 +0000

    Updated main.tf

--- CreateAppServiceEnvironment/main.tf
+++ CreateAppServiceEnvironment/main.tf
@@ -4,7 +4,6 @@ module "app_service_environment" {
   resource_group_name             = var.resource_group_name
   subnet_id                       = var.subnet_id
   internal_load_balancing_mode    = var.internal_load_balancing_mode
-  dns_suffix                      = var.dns_suffix
   disable_tls1_0                  = var.disable_tls1_0
   internal_encryption             = var.internal_encryption
   frontend_ssl_cipher_suite_order = var.frontend_ssl_cipher_suite_order
