7564275da172fd66b54da2452c91629390fc05b2
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-27 12:29:30 +0000

    Updated main.tf

--- Modules/AzureLogicApp/main.tf
+++ Modules/AzureLogicApp/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
 }
 
 data "azurerm_app_service_environment_v3" "ase" {
@@ -10,6 +10,7 @@ data "azurerm_app_service_environment_v3" "ase" {
 data "azurerm_storage_account" "storage" {
   name                = var.storage_account_name
   resource_group_name = var.storage_resource_group_name
+  
 }
 
 data "azurerm_user_assigned_identity" "mi" {
@@ -17,32 +18,33 @@ data "azurerm_user_assigned_identity" "mi" {
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_service_plan" "asp" {
+resource "azurerm_app_service_plan" "asp" {
   name                = var.app_service_plan_name
   location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  os_type             = "Windows"  # Add the required os_type argument
+  resource_group_name = data.azurerm_resource_group.rg.name  
+  kind                = "Windows"
+  reserved            = false
 
-  sku_name            = "I1v2"  # Use a valid SKU name
+  sku {
+    tier = "IsolatedV2"
+    size = "I1V2"
+    capacity = 1
+  }
 
   app_service_environment_id = data.azurerm_app_service_environment_v3.ase.id
 }
 
-resource "azurerm_windows_web_app" "logic_app" {
-  name                        = var.logic_app_name
-  location                    = data.azurerm_resource_group.rg.location
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  service_plan_id             = azurerm_service_plan.asp.id  # Correct reference
-  storage_account_name        = data.azurerm_storage_account.storage.name
-  storage_account_access_key  = data.azurerm_storage_account.storage.primary_access_key
+resource "azurerm_logic_app_standard" "logic_app" {
+  name                = var.logic_app_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+  storage_account_name = data.azurerm_storage_account.storage.name
+  storage_account_access_key = data.azurerm_storage_account.storage.primary_access_key
 
   identity {
     type         = "UserAssigned"
     identity_ids = [data.azurerm_user_assigned_identity.mi.id]
-  }
-
-  site_config {
-    always_on = true
   }
 }
 
