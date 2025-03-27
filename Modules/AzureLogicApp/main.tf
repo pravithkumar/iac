eb5f509a0ca400bc2f4333f09ad8ae2c34afaa07
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-27 05:22:12 +0000

    Updated main.tf

--- Modules/AzureLogicApp/main.tf
+++ Modules/AzureLogicApp/main.tf
@@ -1,27 +1,25 @@
-
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# Reference to the existing ASE
-data "azurerm_app_service_environment_v3" "existing" {
+data "azurerm_app_service_environment_v3" "ase" {
   name                = var.ase_name
   resource_group_name = var.ase_resource_group_name
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_app_service_plan" "asp" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   kind                = "ASEV3"
   reserved            = true
 
@@ -30,16 +28,16 @@ resource "azurerm_app_service_plan" "example" {
     size = "P1v3"
   }
 
-  app_service_environment_id = data.azurerm_app_service_environment_v3.existing.id
+  app_service_environment_id = data.azurerm_app_service_environment_v3.ase.id
 }
 
-resource "azurerm_logic_app_standard" "example" {
+resource "azurerm_logic_app_standard" "logic_app" {
   name                = var.logic_app_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 
   storage_account {
-    name = azurerm_storage_account.example.name
+    name = azurerm_storage_account.storage.name
   }
 }
