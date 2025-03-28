a85ccd45f9dd141962abc9ae8322a580f53602f5
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-28 04:47:17 +0000

    Updated main.tf

--- CreateApiManagement/main.tf
+++ CreateApiManagement/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 
 module "api_management" {
-  source                          = "../Modules/ApiManagement"
+  source                          = "../Modules/api-management"
   api_management_name             = var.api_management_name
   location                        = var.location
   resource_group_name             = var.resource_group_name
@@ -16,7 +16,7 @@ module "api_management" {
 }
 
 module "private_endpoint_api_management" {
-  source                          = "../Modules/PrivateEndpoint"
+  source                          = "../Modules/private-endpoint"
   private_endpoint_name           = var.private_endpoints[0].name
   location                        = var.location
   resource_group_name             = var.resource_group_name
