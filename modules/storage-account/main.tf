8d30ba872e9511ecd8caf7b56843d9b0a1be6729
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-31 08:38:41 +0000

    Updated main.tf

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -33,10 +33,10 @@ resource "azurerm_advanced_threat_protection" "example" {
   enabled            = var.advanced_threat_protection_enabled
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
+// output "storage_account_name" {
+//   value = azurerm_storage_account.sa.name
+// }
 
-output "storage_account_primary_access_key" {
-  value = azurerm_storage_account.sa.primary_access_key
-}
+// output "storage_account_primary_access_key" {
+//   value = azurerm_storage_account.sa.primary_access_key
+// }
