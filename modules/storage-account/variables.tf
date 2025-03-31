c23072a7acf69d7313ad1ff0f0319d5cc317c8d2
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-31 08:39:26 +0000

    Updated variables.tf

--- modules/storage-account/variables.tf
+++ modules/storage-account/variables.tf
@@ -1,5 +1,3 @@
-// modules/storage/variables.tf
-
 variable "resource_group_name" {
   type = string
 }
@@ -20,7 +18,37 @@ variable "account_replication_type" {
   type = string
 }
 
+variable "public_network_access_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "https_traffic_only_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "identity_type" {
+  type    = string
+  default = "SystemAssigned"
+}
+
+variable "advanced_threat_protection_enabled" {
+  type    = bool
+  default = true
+}
+
 variable "tags" {
   type = map(string)
-
 }
+
+// Uncomment if you want to enable soft delete
+// variable "delete_retention_days" {
+//   type    = number
+//   default = 1
+// }
+
+// variable "container_delete_retention_days" {
+//   type    = number
+//   default = 1
+// }
