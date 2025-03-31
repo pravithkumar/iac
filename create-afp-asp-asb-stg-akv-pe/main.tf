741af219a36740149269eedffcd2c5f1d188bf5d
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-31 06:39:40 +0000

    Updated main.tf

--- create-afp-asp-asb-stg-akv-pe/main.tf
+++ create-afp-asp-asb-stg-akv-pe/main.tf
@@ -17,7 +17,7 @@ module "azurerm_linux_function_app" {
   https_only                          = var.https_only
   always_on                           = var.always_on
   tags                                = var.tags
-  depends_on                          = [module.azurerm_service_plan]
+  depends_on                          = [module.azurerm_service_plan,module.azurerm_storage_account]
 }
 
 module "azurerm_service_plan" {
