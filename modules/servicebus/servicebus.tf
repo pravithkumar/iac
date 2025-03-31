bf2a0b5cbec3b341e102c538fd7bda75f9909e60
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-03-31 12:17:40 +0000

    Updated servicebus.tf

--- modules/servicebus/servicebus.tf
+++ modules/servicebus/servicebus.tf
@@ -15,8 +15,8 @@ resource "azurerm_servicebus_namespace" "servicebus" {
   }
 
   network_rule_set {
-    public_network_access_enabled = false
-    trusted_services_allowed      = true
+    public_network_access_enabled = var.public_network_access_enabled
+    trusted_services_allowed      = var.trusted_services_allowed
 }
 
 }
