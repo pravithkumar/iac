4d2bab88cc60e6e15c482d48c2252d4618f2d10b
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-01 07:57:11 +0000

    Updated provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   backend "azurerm" {
+
+    subscription_id      = "795783af-96d3-4629-9161-58de5577ed1e"
+    resource_group_name  = "pravith-vm-jump-box"
+    storage_account_name = "tfstatemsdn"
+    container_name       = "tfstate"
+    key                  = "statefile-fortrea.tfstate"
+    service_arm          = "SC_CCS_400847_Priya_MSDN-2"
+
     // subscription_id       = "795783af-96d3-4629-9161-58de5577ed1e"
     // backendAzureRmResourceGroupName    = "pravith-vm-jump-box"
     // backendAzureRmStorageAccountName   = "tfstatemsdn"
