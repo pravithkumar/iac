1cdff06fe0573b5fbc5c24ee84537d203ad0b860
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-01 07:54:30 +0000

    Updated main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,13 +1,8 @@
-// provider "azurerm" {
-//   alias           = "integ-nprod-001"
-//   subscription_id = "795783af-96d3-4629-9161-58de5577ed1e"
-//   features {}
-// }
 
 module "azurerm_linux_function_app" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                              = "../modules/function-app"
   function_app_name                   = var.function_app_name
   location                            = var.location
@@ -26,9 +21,9 @@ module "azurerm_linux_function_app" {
 }
 
 module "azurerm_service_plan" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                              = "../modules/app-service-plan"
   resource_group_name                 = var.asp_resource_group_name
   location                            = var.location
@@ -40,9 +35,9 @@ module "azurerm_service_plan" {
 }
 
 module "private_endpoint_function_app" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                          = "../modules/private-endpoint"
   private_endpoint_name           = "pe-${var.function_app_name}"
   location                        = var.location
@@ -58,9 +53,9 @@ module "private_endpoint_function_app" {
 }
 
 module "azurerm_storage_account" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   for_each                = { for sa in var.storage_accounts : sa.name => sa }
   source                  = "../modules/storage-account"
   storage_account_name    = each.value.name
@@ -76,9 +71,9 @@ module "azurerm_storage_account" {
 }
 
 module "private_endpoint_storage" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   for_each                        = { for sa in var.storage_accounts : sa.name => sa }
   source                          = "../modules/private-endpoint"
   private_endpoint_name           = "pe-${each.value.name}"
@@ -95,9 +90,9 @@ module "private_endpoint_storage" {
 }
 
 module "servicebus" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                = "../modules/servicebus"
   resource_group_name   = var.resource_group_name
   location              = var.location
@@ -109,9 +104,9 @@ module "servicebus" {
 }
 
 module "private_endpoint_servicebus" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                          = "../modules/private-endpoint"
   private_endpoint_name           = "pe-${var.servicebus_name}"
   location                        = var.location
@@ -127,9 +122,9 @@ module "private_endpoint_servicebus" {
 }
 
 module "azurerm_key_vault" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                              = "../modules/key-vault"
   key_vault_name                      = var.key_vault_name
   location                            = var.location
@@ -152,9 +147,9 @@ module "azurerm_key_vault" {
 }
 
 module "private_endpoint_key_vault" {  
-  // providers = {
-  //   azurerm = azurerm.integ-nprod-001
-  // }
+  providers = {
+    azurerm = azurerm.integ-nprod-001
+  }
   source                          = "../modules/private-endpoint"
   private_endpoint_name           = "pe-${var.key_vault_name}"
   location                        = var.location
