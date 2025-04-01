4179d1e53fd70342a91ab03405e8f2c60d853868
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-01 16:29:33 +0000

    Updated locals.tf

--- iac/locals.tf
+++ iac/locals.tf
@@ -2,6 +2,5 @@ locals {
   app_name     = var.app_name
   environment  = var.environment
   location-test     = var.location-test
-  function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location-test}-001"
-  resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location-test}-001"
+  function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location-test}-001"  
 }
