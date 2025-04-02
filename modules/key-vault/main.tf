1d370a8d044dc556dff989fe82af3a2a1425e677
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-02 11:44:41 +0000

    Updated main.tf

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -3,8 +3,7 @@ resource "azurerm_key_vault" "this" {
   name                            = var.azurerm_key_vault_name
   location                        = var.location
   resource_group_name             = var.resource_group_name
-  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
-  tenant_id                       = var.tenant_id
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption  
   soft_delete_retention_days      = var.soft_delete_retention_days
   purge_protection_enabled        = var.purge_protection_enabled
   tags                            = var.tags
