b1b19900bdeb471aa8fbacce8fff98924bf37695
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-02 11:27:26 +0000

    Updated main.tf

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -1,36 +1,31 @@
+resource "azurerm_key_vault" "this" {
 
-data "azurerm_client_config" "current" {}
-
-
-
-resource "azurerm_key_vault" "key_vault" {
-  name                            = var.key_vault_name
+  name                            = var.azurerm_key_vault_name
   location                        = var.location
   resource_group_name             = var.resource_group_name
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
-  sku_name                        = var.kvskuname
-  purge_protection_enabled        = var.kvpurge_protection_enabled
-  enable_rbac_authorization       = var.kvrbac_authorization
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  tenant_id                       = var.tenant_id
+  soft_delete_retention_days      = var.soft_delete_retention_days
+  purge_protection_enabled        = var.purge_protection_enabled
+  tags                            = var.tags
+  sku_name                        = var.sku_name
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  enabled_for_deployment          = var.enabled_for_deployment
+  enable_rbac_authorization       = var.enable_rbac_authorization
   public_network_access_enabled   = var.public_network_access_enabled
-  soft_delete_retention_days      = var.kvsoft_delete_retention_days
-  enabled_for_deployment          = var.enabled_for_deployment 
-  enabled_for_disk_encryption     = var.enabledfordiskencryption
-  enabled_for_template_deployment = var.enabledfortemplatedeployment
 
-  network_acls {
-    default_action = var.kvnetdefaultaction
-    bypass         = var.kvnetaclbypass
+  dynamic "network_acls" {
+    for_each = var.network_acls != null ? var.network_acls : []
 
-    ip_rules = var.kvip_rules
-
-   
+    content {
+      bypass                     = network_acls.value.bypass
+      default_action             = network_acls.value.default_action
+      ip_rules                   = network_acls.value.ip_rules
+      virtual_network_subnet_ids = network_acls.value.virtual_network_subnet_ids
+    }
   }
 
-  timeouts {
-    create = var.kvtimeoutcreate
-    update = var.kvtimeoutupdate
-    delete = var.kvtimeoutdelete
+  lifecycle {
+    ignore_changes = [tags]
   }
-
-  tags = var.tags
 }
