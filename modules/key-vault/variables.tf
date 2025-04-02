92ae3f5db3370b8411bc946866ae41a3d1c5c0a0
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-02 11:28:36 +0000

    Updated variables.tf

--- modules/key-vault/variables.tf
+++ modules/key-vault/variables.tf
@@ -1,106 +1,74 @@
-variable "key_vault_name" {
-  description = "The name of the Key Vault"
+variable "azurerm_key_vault_name" {
+  description = "The name of the Azure Key Vault."
   type        = string
 }
 
 variable "location" {
-  description = "The location of the resources"
+  description = "The location where the resources will be created."
   type        = string
-  default     = "West Europe"
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "The name of the resource group."
   type        = string
 }
 
-variable "kvskuname" {
-  description = "The SKU name of the Key Vault"
+variable "enabled_for_disk_encryption" {
+  description = "Specifies whether Azure Disk Encryption is permitted."
+  type        = bool
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for the Azure subscription."
   type        = string
-  default     = "standard"
 }
 
-variable "kvpurge_protection_enabled" {
-  description = "Enable purge protection"
-  type        = bool
-  default     = true
-}
-
-variable "kvrbac_authorization" {
-  description = "Enable RBAC authorization"
-  type        = bool
-  default     = false
-}
-
-variable "kvsoft_delete_retention_days" {
-  description = "Soft delete retention days"
+variable "soft_delete_retention_days" {
+  description = "The number of days to retain deleted vaults."
   type        = number
-  default     = 7
 }
 
-variable "enabled_for_deployment" {
-  description = "Enable Key Vault for deployment"
+variable "purge_protection_enabled" {
+  description = "Specifies whether purge protection is enabled."
   type        = bool
-  default     = false
-}
-
-variable "enabledfordiskencryption" {
-  description = "Enable Key Vault for disk encryption"
-  type        = bool
-  default     = false
-}
-
-variable "enabledfortemplatedeployment" {
-  description = "Enable Key Vault for template deployment"
-  type        = bool
-  default     = false
-}
-
-variable "public_network_access_enabled" {
-  description = "Enable public_network_access_enabled"
-  type        = bool
-  default     = false
-}
-
-variable "kvnetdefaultaction" {
-  description = "Default action for network ACLs"
-  type        = string
-  default     = "Deny"
-}
-
-variable "kvnetaclbypass" {
-  description = "Bypass for network ACLs"
-  type        = string
-  default     = "AzureServices"
-}
-
-variable "kvip_rules" {
-  description = "IP rules for network ACLs"
-  type        = list(string)
-  default     = []
-}
-
-variable "kvtimeoutcreate" {
-  description = "Timeout for creating the Key Vault"
-  type        = string
-  default     = "30m"
-}
-
-variable "kvtimeoutupdate" {
-  description = "Timeout for updating the Key Vault"
-  type        = string
-  default     = "30m"
-}
-
-variable "kvtimeoutdelete" {
-  description = "Timeout for deleting the Key Vault"
-  type        = string
-  default     = "30m"
 }
 
 variable "tags" {
-  description = "Tags for the Key Vault"
+  description = "A map of tags to assign to the resource."
   type        = map(string)
-  default     = {}
 }
 
+variable "sku_name" {
+  description = "The SKU name of the Key Vault."
+  type        = string
+}
+
+variable "enabled_for_template_deployment" {
+  description = "Specifies whether Azure Resource Manager template deployment is enabled."
+  type        = bool
+}
+
+variable "enabled_for_deployment" {
+  description = "Specifies whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault."
+  type        = bool
+}
+
+variable "enable_rbac_authorization" {
+  description = "Specifies whether RBAC authorization is enabled."
+  type        = bool
+}
+
+variable "public_network_access_enabled" {
+  description = "Specifies whether public network access is enabled."
+  type        = bool
+}
+
+variable "network_acls" {
+  description = "A list of network ACLs."
+  type = list(object({
+    bypass                     = string
+    default_action             = string
+    ip_rules                   = list(string)
+    virtual_network_subnet_ids = list(string)
+  }))
+}
