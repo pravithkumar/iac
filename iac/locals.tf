274d26271fbf6cb19838e11080d80b4a8829878a
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-02 08:30:37 +0000

    Updated locals.tf

--- iac/locals.tf
+++ iac/locals.tf
@@ -4,4 +4,7 @@ locals {
   location-test     = var.location-test
   function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location-test}-001"  
   resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location-test}-001"
+  service_plan_name       = "asp-${local.app_name}-${local.environment}-${local.location-test}-001"
+  servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location-test}-001"
+  key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location-test}-001"
 }
