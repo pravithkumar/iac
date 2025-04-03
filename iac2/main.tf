a3326b6f9f631ef2b3393f3c37a811ca1af52e11
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-03 13:25:16 +0000

    Updated main.tf

--- iac2/main.tf
+++ iac2/main.tf
@@ -4,14 +4,14 @@ provider "azurerm" {
 
 module "infrastructure" {
   source = "../modules/rg-vnet-dns"
-  providers                           =  {azurerm = azurerm.integ-nprod-001}
+  providers = { azurerm = azurerm.integ-nprod-001 }
+
   resource_group_1_name = var.resource_group_1_name
   resource_group_2_name = var.resource_group_2_name
   resource_group_3_name = var.resource_group_3_name
   location              = var.location
   vnet_name             = var.vnet_name
   vnet_address_space    = var.vnet_address_space
-  subnet_name           = var.subnet_name
-  subnet_address_prefixes = var.subnet_address_prefixes
+  subnets               = var.subnets
   private_dns_zones     = var.private_dns_zones
 }
