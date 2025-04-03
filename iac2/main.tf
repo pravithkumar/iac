9285d8708105806912adb902682a33e7e76d2a36
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-03 09:07:45 +0000

    Updated main.tf

---
+++ iac2/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+module "infrastructure" {
+  source = "./modules/private_dns_zone"
+
+  resource_group_1_name = var.resource_group_1_name
+  resource_group_2_name = var.resource_group_2_name
+  resource_group_3_name = var.resource_group_3_name
+  location              = var.location
+  vnet_name             = var.vnet_name
+  vnet_address_space    = var.vnet_address_space
+  subnet_name           = var.subnet_name
+  subnet_address_prefixes = var.subnet_address_prefixes
+  private_dns_zones     = var.private_dns_zones
+}
