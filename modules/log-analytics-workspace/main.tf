f55bfb032808fa9b5edf18cf234323e998233ea1
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-03 14:35:47 +0000

    Updated main.tf

--- modules/log-analytics-workspace/main.tf
+++ modules/log-analytics-workspace/main.tf
@@ -3,10 +3,5 @@ resource "azurerm_log_analytics_workspace" "law" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-    dynamic "retention" {
-      for_each = var.retention_in_days != null ? [var.retention_in_days] : []
-      content {
-      retention_in_days = var.retention_in_days
-    }
-    //   retention_in_days   = 30
+  retention_in_days   = var.retention_in_days != null ? var.retention_in_days : null
 }
