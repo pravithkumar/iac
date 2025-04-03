c0351c2958a7413bdababa5851778a3caddb8f87
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-03 14:18:50 +0000

    Updated main.tf

--- modules/rg-vnet-dns/main.tf
+++ modules/rg-vnet-dns/main.tf
@@ -21,12 +21,12 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
- count               = length(var.subnet_names)
-  name                = var.subnet_names[count.index]
-  resource_group_name = var.resource_group_name
+  count                = length(var.subnet_names)
+  name                 = var.subnet_names[count.index]
+  resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes    = [var.subnet_prefixes[count.index]]
-  depends_on           = [azurerm_virtual_network.vnet]
+  address_prefixes     = [var.subnet_prefixes[count.index]]
+  depends_on            = [azurerm_virtual_network.vnet]
 }
 
 resource "azurerm_private_dns_zone" "private_dns_zone" {
