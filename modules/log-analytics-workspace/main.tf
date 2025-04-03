3d5393b3da5377d5b224394894d198dc854d9bec
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-03 15:37:05 +0000

    Updated main.tf

--- modules/log-analytics-workspace/main.tf
+++ modules/log-analytics-workspace/main.tf
@@ -4,4 +4,11 @@ resource "azurerm_log_analytics_workspace" "law" {
   resource_group_name = var.resource_group_name
   sku                 = var.sku
   retention_in_days   = var.retention_in_days != null ? var.retention_in_days : null
+  dynamic "identity" {
+    for_each = var.identity_type == null ? [] : [1] # Adds identity block only if needed
+    content {
+      type         = var.identity_type
+      identity_ids = var.identity_type == "UserAssigned" ? var.user_identity_ids : null
+    }
+  }
 }
