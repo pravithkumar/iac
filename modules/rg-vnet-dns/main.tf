eb3aaf5c212d81d2c4e94a463f3ee9b2d830c35d
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-03 10:39:43 +0000

    Updated main.tf

--- modules/rg-vnet-dns/main.tf
+++ modules/rg-vnet-dns/main.tf
@@ -21,11 +21,13 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg1.name
+  for_each            = var.subnets
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.subnet_address_prefixes
+  address_prefixes    = each.value.address_prefixes
 }
+
 
 resource "azurerm_private_dns_zone" "private_dns_zone" {
   for_each            = var.private_dns_zones
