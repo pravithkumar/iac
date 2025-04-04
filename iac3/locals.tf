159596d16e34c48b7eb1602269f8feb51bf9c2cc
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-04 10:40:59 +0000

    Added locals.tf

---
+++ iac3/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  app_name     = var.app_name
+  environment  = var.environment
+  location_test = var.location_test
+  function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location_test}-001"  
+  resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-001"
+  asp_service_plan_name   = "asp-${local.app_name}-${local.environment}-${local.location_test}-001"
+  servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location_test}-001"
+  key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location_test}-001"
+  storage_account_name_1  = "stg${local.app_name}${local.environment}${local.location_test}001"
+  storage_account_name_2  = "stg${local.app_name}${local.environment}${local.location_test}002"
+}
