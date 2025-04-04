e6f77ce4dfaecd76563dc343a9a4b0ea14af7137
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-04 11:53:38 +0000

    Updated main.tf

--- iac2/main.tf
+++ iac2/main.tf
@@ -13,3 +13,23 @@ module "infrastructure" {
   subnets               = var.subnets
   private_dns_zones     = var.private_dns_zones
 }
+
+module "logworkspace" {
+  source     = "..modules/log-analytics-workspace"
+  law_name                = var.loganalytics.name
+  location            = var.location
+  resource_group_name = var.loganalytics.resource_group_name
+  sku                 = var.loganalytics.sku
+  retention_in_days = var.loganalytics.retention_in_days
+  identity = var.loganalytics.identity
+}
+
+module "app_insight" {
+  source = ".../modules/application-insights"
+  appinsights_name    = var.appinsights.name
+  location            = var.location
+  resource_group_name = var.appinsights.resource_group_name
+  law_workspace_id    = var.appinsights.law_workspace_id
+  application_type    = var.appinsights.application_type
+  retention_in_days   = var.appinsights.retention_in_days
+}
