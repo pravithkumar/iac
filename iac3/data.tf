cdfd6d66ff7bfc86565904631f7e5dee2ff2a00f
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-04 12:56:30 +0000

    Updated data.tf

--- iac3/data.tf
+++ iac3/data.tf
@@ -8,7 +8,7 @@ data "azurerm_subnet" "default_subnet" {
 
 data "azurerm_storage_account" "storage" {
   provider             = azurerm.integ-mgmt-001
-  name                = var.storage_account_name
+  name                = local.storage_account_name_1
   resource_group_name = local.resource_group_name
   
 }
