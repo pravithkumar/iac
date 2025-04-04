d6b34236002ea4dcef1716e35f040ce8389a3a98
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-04 11:31:13 +0000

    Updated variables.tf

--- iac3/variables.tf
+++ iac3/variables.tf
@@ -108,3 +108,31 @@ variable "topic_max_size" {
   type = number
 }
 
+
+###########
+
+variable "internal_load_balancing_mode" {
+  description = "The internal load balancing mode"
+  type        = string
+}
+variable "disable_tls1_0" {
+  description = "Disable TLS 1.0 setting"
+  type        = string
+  default     = "1"
+}
+variable "internal_encryption" {
+  description = "Internal encryption setting"
+  type        = string
+  default     = "true"
+}
+variable "frontend_ssl_cipher_suite_order" {
+  description = "Front-end SSL cipher suite order"
+  type        = string
+  default     = "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+}
+
+variable "storage_account_access_key" {
+  description = "The access key of the existing storage account"
+  type        = string
+}
+
