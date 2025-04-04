70dd0e789a430e88308a87f6c393cd0a2dd6d89a
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-04 08:54:34 +0000

    Updated data.tf

--- iac/data.tf
+++ iac/data.tf
@@ -1,6 +1,6 @@
 data "azurerm_subnet" "default_subnet" {
   provider             = azurerm.integ-mgmt-001
-  name                 = "default"
+  name                 = "subnet1"
   virtual_network_name = "vnet-int-dev-esu-001"
   resource_group_name  = "rg-int-vnet-esu-001"
 }
