f0ee93fc7b4288897f9c53ce0cd5e9a600dfc6e3
Pravith.Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-04 08:54:01 +0000

    Updated data.tf

--- iac/data.tf
+++ iac/data.tf
@@ -1,36 +1,36 @@
 data "azurerm_subnet" "default_subnet" {
   provider             = azurerm.integ-mgmt-001
   name                 = "default"
-  virtual_network_name = "vnet-fortrea-01"
-  resource_group_name  = "rg-fortrea-delete-01"
+  virtual_network_name = "vnet-int-dev-esu-001"
+  resource_group_name  = "rg-int-vnet-esu-001"
 }
 
 data "azurerm_private_dns_zone" "function_app_dns" {
   provider            = azurerm.integ-mgmt-001
   name                = "privatelink.azurewebsites.net"
-  resource_group_name = "rg-fortrea-delete-01"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "servicebus_dns" {
   provider            = azurerm.integ-mgmt-001
   name                = "privatelink.servicebus.windows.net"
-  resource_group_name = "rg-fortrea-delete-01"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "key_vault_dns" {
   provider            = azurerm.integ-mgmt-001
   name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = "rg-fortrea-delete-01"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "storageaccount1_dns" {
   provider            = azurerm.integ-mgmt-001
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-fortrea-delete-01"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "storageaccount2_dns" {
   provider            = azurerm.integ-mgmt-001
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-fortrea-delete-01"
+  resource_group_name = "rg-int-dns-esu-002"
 }
