0a2511575b3611a704e7f295c943a730ea7a7598
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-04 07:08:54 +0000

    Updated main.tf

--- iac2/main.tf
+++ iac2/main.tf
@@ -6,9 +6,7 @@ module "infrastructure" {
   source    = "../modules/rg-vnet-dns"
   providers = { azurerm = azurerm.integ-nprod-001 }
 
-  resource_group_1_name = var.resource_group_1_name
-  resource_group_2_name = var.resource_group_2_name
-  resource_group_3_name = var.resource_group_3_name
+  resource_groups       = var.resource_groups
   location              = var.location
   vnet_name             = var.vnet_name
   vnet_address_space    = var.vnet_address_space
