7d72ebf0b54941047fe649cc98576aecd4f5f40a
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-08 07:54:19 +0000

    Updated locals.tf

--- iac4/locals.tf
+++ iac4/locals.tf
@@ -3,7 +3,7 @@ locals {
   environment  = var.environment
   location_test = var.location_test
 
- 
+  resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-001"
   key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location_test}-003"
 
 }
