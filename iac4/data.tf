ebb8d9ea06c19b2901b0be70d5f46d00bc82f15d
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-08 07:41:32 +0000

    Updated data.tf

--- iac4/data.tf
+++ iac4/data.tf
@@ -13,17 +13,7 @@ data "azurerm_subnet" "delegated_subnet" {
   resource_group_name  = "rg-int-vnet-esu-001"
 }
 
-data "azurerm_private_dns_zone" "function_app_dns" {
-  provider            = azurerm.integ-mgmt-001
-  name                = "privatelink.azurewebsites.net"
-  resource_group_name = "rg-int-dns-esu-002"
-}
 
-data "azurerm_private_dns_zone" "servicebus_dns" {
-  provider            = azurerm.integ-mgmt-001
-  name                = "privatelink.servicebus.windows.net"
-  resource_group_name = "rg-int-dns-esu-002"
-}
 
 data "azurerm_private_dns_zone" "key_vault_dns" {
   provider            = azurerm.integ-mgmt-001
@@ -31,20 +21,3 @@ data "azurerm_private_dns_zone" "key_vault_dns" {
   resource_group_name = "rg-int-dns-esu-002"
 }
 
-data "azurerm_private_dns_zone" "storageaccount1_dns" {
-  provider            = azurerm.integ-mgmt-001
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-int-dns-esu-002"
-}
-
-data "azurerm_private_dns_zone" "storageaccount2_dns" {
-  provider            = azurerm.integ-mgmt-001
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-int-dns-esu-002"
-}
-
-data "azurerm_private_dns_zone" "api_management_dns" {
-  provider            = azurerm.integ-mgmt-001
-  name                = "privatelink.azure-api.net"
-  resource_group_name = "rg-int-dns-esu-002"
-}
