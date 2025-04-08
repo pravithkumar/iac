b341eb810d5ed2c48631fdb78b7cc1ee85d349a1
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-08 07:58:10 +0000

    Updated variables.tf

--- iac4/variables.tf
+++ iac4/variables.tf
@@ -6,6 +6,20 @@ variable "tags" {
   type = map(string)
 }
 
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, prod)"
+  type        = string
+}
+
+variable "location_test" {
+  description = "The Azure region (e.g., eastus, westus)"
+  type        = string
+}
 
 
 
