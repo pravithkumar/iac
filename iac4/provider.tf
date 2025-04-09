e0c2c29ea0619e2cae2c9bef1d10b913a21b81af
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-09 10:59:18 +0000

    Updated provider.tf

--- iac4/provider.tf
+++ iac4/provider.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=4.7.0"
-    }
-  }
+
 
   backend "azurerm" {
     subscription_id      = "795783af-96d3-4629-9161-58de5577ed1e"  
