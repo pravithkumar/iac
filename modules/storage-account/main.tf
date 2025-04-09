eca0cbd84dd6ab883a91c1e10cdbf2f438ebc1a7
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-09 10:18:48 +0000

    Updated main.tf

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,10 +1,6 @@
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.rg.name
+  resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
