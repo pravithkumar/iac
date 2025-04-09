8538be321bb38323cd03388485f4e1a88b5850b4
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-09 10:00:51 +0000

    Updated servicebus.tf

--- modules/servicebus/servicebus.tf
+++ modules/servicebus/servicebus.tf
@@ -8,10 +8,10 @@ resource "azurerm_servicebus_namespace" "servicebus" {
   premium_messaging_partitions = var.sku == "Premium" ? var.premium_messaging_partitions : null
 
   dynamic "identity" {
-  for_each = var.identity != null ? [1] : []
+  for_each = var.identity_type != null ? [1] : []
   content {
-    type       = var.identity.type
-    identity_ids = var.identity.type == "UserAssigned" ? var.identity.identity_ids : null
+    type       = var.identity_type
+    identity_ids = var.identity_type == "UserAssigned" ? var.identity_ids : []
   }
 }
 
