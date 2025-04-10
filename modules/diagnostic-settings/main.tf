58c98c72c19fe2e5a457a6fea951921bf88e90e4
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-10 08:11:44 +0000

    Updated main.tf

--- modules/diagnostic-settings/main.tf
+++ modules/diagnostic-settings/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_diagnostic_setting" "diagnostic" {
-  count             =  var.enable_monitor ? [1] : []
+  count             =  var.enable_monitoring ? [1] : []
   name               = var.monitor_diagnostic_name
   target_resource_id = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
