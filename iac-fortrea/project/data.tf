3a46a0f57256ce9efd10db70604f15d898610404
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 11:19:41 +0000

    Updated data.tf

--- iac-fortrea/project/data.tf
+++ iac-fortrea/project/data.tf
@@ -1,56 +1,56 @@
 data "azurerm_subnet" "default_subnet" {
   provider             = azurerm.integ-nprod-001
-  name                 = "subnet1"
-  virtual_network_name = "vnet-int-dev-eus-001"
-  resource_group_name  = "rg-int-vnet-dev-eus-001"
+  name                 = "default"
+  virtual_network_name = "vnet-fort-dev-eus-001"
+  resource_group_name  = "rg-fort-dev-eus-004"
 }
 
 
 data "azurerm_subnet" "delegated_subnet" {
   provider             = azurerm.integ-nprod-001
-  name                 = "subnet2"
-  virtual_network_name = "vnet-int-dev-eus-001"
-  resource_group_name  = "rg-int-vnet-dev-eus-001"
+  name                 = "default2"
+  virtual_network_name = "vnet-fort-dev-eus-001"
+  resource_group_name  = "rg-fort-dev-eus-004"
 }
 
 data "azurerm_application_insights" "ai" {
-  provider            = azurerm.integ-nprod-001
-  name                = "ai-int-dev-eus-001"
-  resource_group_name = "rg-int-apps-dev-eus-003"
+  provider            = azurerm.integ-mgmt-001
+  name                = "appinsights-int-esu-001"
+  resource_group_name = "rg-int-apps-esu-003"
 }
 
 data "azurerm_private_dns_zone" "function_app_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.azurewebsites.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "servicebus_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.servicebus.windows.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "key_vault_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "storageaccount1_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "storageaccount2_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
 
 data "azurerm_private_dns_zone" "api_management_dns" {
-  provider            = azurerm.integ-nprod-001
+  provider            = azurerm.integ-mgmt-001
   name                = "privatelink.azure-api.net"
-  resource_group_name = "rg-int-dns-dev-eus-002"
+  resource_group_name = "rg-int-dns-esu-002"
 }
