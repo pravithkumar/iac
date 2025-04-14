e9b6ae7a5747fe289883f07c0b150d6a1c2a5759
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 05:24:08 +0000

    Updated main.tf

---
+++ functionapp/main.tf
@@ -0,0 +1,65 @@
+module "resource_group" {
+  providers                           =  {azurerm = azurerm.integ-nprod-001}
+  source                              = "../modules/resource-group"
+  resource_group_name                 = local.resource_group_name
+  location                            = var.location
+}
+
+module "azurerm_linux_function_app" {  
+  providers                           =  {azurerm = azurerm.integ-nprod-001}
+  source                              = "../modules/function-app"
+  function_app_name                   = local.function_app_name
+  location                            = var.location
+  resource_group_name                 = local.resource_group_name
+  storage_account_name                = local.storage_account_name_1
+  storage_account_resource_group_name = local.resource_group_name
+  app_service_plan_name               = local.asp_service_plan_name
+  runtime                             = var.runtime
+  runtime_version                     = var.runtime_version
+  https_only                          = true
+  always_on                           = var.always_on
+  tags                                = var.tags
+  identity_type                       = "SystemAssigned"
+  identity_ids                        = []
+  enable_app_insights                 = true
+  appinsights_instrumentationkey      = var.enable_app_insights ? data.azurerm_application_insights.ai.instrumentation_key : null
+  applicationinsights_connectionstring = var.enable_app_insights ? data.azurerm_application_insights.ai.connection_string : null
+  depends_on                          = [module.azurerm_service_plan, module.azurerm_storage_account_1]
+}
+
+module "private_endpoint_function_app" {  
+  providers                          =  {azurerm = azurerm.integ-nprod-001}
+  source                             = "../modules/private-endpoint"
+  private_endpoint_name              = "pe-${local.function_app_name}"
+  location                           = var.location
+  resource_group_name                = local.resource_group_name
+  subnet_id                          = data.azurerm_subnet.default_subnet.id
+  private_service_connection_name    = "${local.function_app_name}-psc"
+  private_connection_resource_id     = module.azurerm_linux_function_app.function_app_id
+  subresource_names                  = ["sites"]
+  is_manual_connection               = false
+  private_dns_zone_group_name        = "private-dns-zone-group"
+  private_dns_zone_ids               = [data.azurerm_private_dns_zone.function_app_dns.id]
+  depends_on                         = [module.azurerm_linux_function_app]
+}
+
+module "azurerm_service_plan" {  
+  providers                           =  {azurerm = azurerm.integ-nprod-001}
+  source                              = "../modules/app-service-plan"
+  resource_group_name                 = local.resource_group_name
+  service_plan_name                   = local.asp_service_plan_name
+  location                            = var.location
+  os_type                             = var.os_type
+  aspsku_name                         = var.aspsku_name  
+  worker_count                        = var.worker_count
+  tags                                = var.tags
+}
+
+module "diagnostic_setting" {
+    providers                         =  {azurerm = azurerm.integ-nprod-001}
+    source                            = "../modules/diagnostic-settings"
+    enable_monitoring                 = true
+    monitor_diagnostic_name           = local.monitor_diagnostic_name
+    target_resource_id                = module.azurerm_linux_function_app.function_app_id
+    log_analytics_workspace_id        = 
+}
