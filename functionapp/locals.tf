1f1c94d361a58831d7b379b9157744929636724e
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 06:14:57 +0000

    Updated locals.tf

--- functionapp/locals.tf
+++ functionapp/locals.tf
@@ -6,6 +6,7 @@ locals {
   app_service_plan_name        = "asp-${local.app_name}-${local.environment}-${local.location_test}-005"
 
   function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location_test}-005"  
+  monitor_diagnostic_name = "mds-${local.app_name}-${local.environment}-${local.location_test}-005" 
   resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-005"
   asp_service_plan_name   = "asp-${local.app_name}-${local.environment}-${local.location_test}-005"
   storage_account_name_1  = "stg${local.app_name}${local.environment}${local.location_test}005"
