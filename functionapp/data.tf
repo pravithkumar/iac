f598abad110006285e329860a38700f650b7f43b
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 06:26:06 +0000

    Updated data.tf

--- functionapp/data.tf
+++ functionapp/data.tf
@@ -15,3 +15,18 @@ data "azurerm_private_dns_zone" "function_app_dns" {
   name                = "privatelink.azurewebsites.net"
   resource_group_name = "rg-int-dns-esu-002"
 }
+
+data "azurerm_subnet" "default_subnet" {
+  provider             = azurerm.integ-nprod-001
+  name                 = "default"
+  virtual_network_name = "vnet-fort-dev-eus-001"
+  resource_group_name  = "rg-fort-dev-eus-004"
+}
+
+
+data "azurerm_subnet" "delegated_subnet" {
+  provider             = azurerm.integ-nprod-001
+  name                 = "default2"
+  virtual_network_name = "vnet-fort-dev-eus-001"
+  resource_group_name  = "rg-fort-dev-eus-004"
+}
