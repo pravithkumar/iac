98bc6ed704c7d0f54f42ed6b4c81892ae0ce8e13
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 06:12:31 +0000

    Updated variables.tf

---
+++ functionapp/variables.tf
@@ -0,0 +1,76 @@
+variable "location" {
+  type = string
+}
+
+variable "runtime" {
+  type = string
+}
+
+variable "runtime_version" {
+  type = string
+}
+
+
+
+variable "always_on" {
+  type = bool
+}
+
+variable "os_type" {
+  type = string
+}
+
+variable "aspsku_name" {
+  type = string
+}
+
+variable "worker_count" {
+  type = number
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, prod)"
+  type        = string
+}
+
+variable "location_test" {
+  description = "The Azure region (e.g., eastus, westus)"
+  type        = string
+}
+
+variable "enable_app_insights" {
+  type        = bool
+  description = "Whether to enable Application Insights integration for the Function App."
+  default     = false
+}
+
+variable "app_insights_resource_group_name" {
+  description = "app_insights_name  RG"
+  type        = string
+}
+
+variable "app_insights_name" {
+  description = "app_insights_name  existing "
+  type        = string
+}
