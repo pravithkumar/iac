6e35a4c79a105d4bc7b6e596d810be071e7c575f
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-14 09:51:35 +0000

    Updated main.tf

--- functionapp/main.tf
+++ functionapp/main.tf
@@ -72,7 +72,7 @@ module "role_assignment_storage_contributor" {
   providers                         =  {azurerm = azurerm.integ-nprod-001}
   source                            = "../modules/role_assignments"
   scope                             = module.azurerm_storage_account_1.id
-  role_definition_name              = "Storage Account Contributor"
+  role_definition_name              = "Storage Blob Data Owner"
   principal_id                      = module.azurerm_linux_function_app.principal_id
 }
 
