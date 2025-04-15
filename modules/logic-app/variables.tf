5a4cd1af454aca8ae68f1059150ed72dca7ce2c3
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-15 09:07:57 +0000

    Updated variables.tf

--- modules/logic-app/variables.tf
+++ modules/logic-app/variables.tf
@@ -65,3 +65,19 @@ variable "sku_name" {
   description = "The SKU name for the App Service Plan (e.g., B1, P1v2)."
   type        = string
 }
+
+variable "appinsights_instrumentationkey" {
+  type        = string
+  description = "Instrumentation Key for Application Insights"
+}
+
+variable "applicationinsights_connectionstring" {
+  type        = string
+  description = "Connection String for Application Insights"
+}
+
+variable "app_settings" {
+  type        = map(string)
+  description = "A map of application settings to apply to the Function App."
+  default     = {}
+}
