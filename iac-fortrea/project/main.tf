a074ced458b712839e269c8b9c1b410f46ab3ba5
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-15 13:42:05 +0000

    Updated main.tf

--- iac-fortrea/project/main.tf
+++ iac-fortrea/project/main.tf
@@ -6,76 +6,76 @@ module "resource_group" {
   tags                                =var.tags
 }
 
-module "azurerm_linux_function_app" {  
-  providers                           =  {azurerm = azurerm.integ-nprod-001}
-  source                              = "../modules/function-app"
-  function_app_name                   = local.function_app_name
-  location                            = var.location
-  resource_group_name                 = local.resource_group_name
-  storage_account_name                = local.storage_account_name_2
-  storage_account_resource_group_name = local.resource_group_name
-  app_service_plan_name               = local.asp_service_plan_name
-  runtime                             = var.runtime
-  runtime_version                     = var.runtime_version
-  https_only                          = true
-  always_on                           = var.always_on
-  tags                                = var.tags
-  identity_type                       = "SystemAssigned"
-  identity_ids                        = []
-  app_settings                        = var.app_settings
-  enable_app_insights                 = true
-  appinsights_instrumentationkey      = data.azurerm_application_insights.ai.instrumentation_key
-  applicationinsights_connectionstring = data.azurerm_application_insights.ai.connection_string
-  depends_on                          = [module.azurerm_service_plan, module.azurerm_storage_account_1,module.resource_group]
-}
+// module "azurerm_linux_function_app" {  
+//   providers                           =  {azurerm = azurerm.integ-nprod-001}
+//   source                              = "../modules/function-app"
+//   function_app_name                   = local.function_app_name
+//   location                            = var.location
+//   resource_group_name                 = local.resource_group_name
+//   storage_account_name                = local.storage_account_name_2
+//   storage_account_resource_group_name = local.resource_group_name
+//   app_service_plan_name               = local.asp_service_plan_name
+//   runtime                             = var.runtime
+//   runtime_version                     = var.runtime_version
+//   https_only                          = true
+//   always_on                           = var.always_on
+//   tags                                = var.tags
+//   identity_type                       = "SystemAssigned"
+//   identity_ids                        = []
+//   app_settings                        = var.app_settings
+//   enable_app_insights                 = true
+//   appinsights_instrumentationkey      = data.azurerm_application_insights.ai.instrumentation_key
+//   applicationinsights_connectionstring = data.azurerm_application_insights.ai.connection_string
+//   depends_on                          = [module.azurerm_service_plan, module.azurerm_storage_account_1,module.resource_group]
+// }
 
-module "private_endpoint_function_app" {  
-  providers                          =  {azurerm = azurerm.integ-nprod-001}
-  source                             = "../modules/private-endpoint"
-  private_endpoint_name              = "pe-${local.function_app_name}"
-  location                           = var.location
-  resource_group_name                = local.resource_group_name
-  subnet_id                          = data.azurerm_subnet.default_subnet.id
-  private_service_connection_name    = "${local.function_app_name}-psc"
-  private_connection_resource_id     = module.azurerm_linux_function_app.function_app_id
-  subresource_names                  = ["sites"]
-  is_manual_connection               = false
-  private_dns_zone_group_name        = "private-dns-zone-group"
-  private_dns_zone_ids               = [data.azurerm_private_dns_zone.function_app_dns.id]
-  depends_on                         = [module.azurerm_linux_function_app,module.resource_group]
-}
+// module "private_endpoint_function_app" {  
+//   providers                          =  {azurerm = azurerm.integ-nprod-001}
+//   source                             = "../modules/private-endpoint"
+//   private_endpoint_name              = "pe-${local.function_app_name}"
+//   location                           = var.location
+//   resource_group_name                = local.resource_group_name
+//   subnet_id                          = data.azurerm_subnet.default_subnet.id
+//   private_service_connection_name    = "${local.function_app_name}-psc"
+//   private_connection_resource_id     = module.azurerm_linux_function_app.function_app_id
+//   subresource_names                  = ["sites"]
+//   is_manual_connection               = false
+//   private_dns_zone_group_name        = "private-dns-zone-group"
+//   private_dns_zone_ids               = [data.azurerm_private_dns_zone.function_app_dns.id]
+//   depends_on                         = [module.azurerm_linux_function_app,module.resource_group]
+// }
 
-module "diagnostic_setting_fa" {
-  providers                         =  {azurerm = azurerm.integ-nprod-001}
-  source                            = "../modules/diagnostic-settings"
-  enable_monitoring                 = true
-  monitor_diagnostic_name           = local.monitor_diagnostic_name_1
-  target_resource_id                = module.azurerm_linux_function_app.function_app_id
-  log_analytics_workspace_id        = data.azurerm_log_analytics_workspace.la.id
-  category                          = "FunctionAppLogs"
-  depends_on                         = [module.azurerm_linux_function_app]
-}
+// module "diagnostic_setting_fa" {
+//   providers                         =  {azurerm = azurerm.integ-nprod-001}
+//   source                            = "../modules/diagnostic-settings"
+//   enable_monitoring                 = true
+//   monitor_diagnostic_name           = local.monitor_diagnostic_name_1
+//   target_resource_id                = module.azurerm_linux_function_app.function_app_id
+//   log_analytics_workspace_id        = data.azurerm_log_analytics_workspace.la.id
+//   category                          = "FunctionAppLogs"
+//   depends_on                         = [module.azurerm_linux_function_app]
+// }
 
-module "role_assignment_storage_contributor" {
-  providers                         =  {azurerm = azurerm.integ-nprod-001}
-  source                            = "../modules/role_assignments"
-  scope                             = module.azurerm_storage_account_2.id
-  role_definition_name              = "Storage Blob Data Owner"
-  principal_id                      = module.azurerm_linux_function_app.principal_id
-}
+// module "role_assignment_storage_contributor" {
+//   providers                         =  {azurerm = azurerm.integ-nprod-001}
+//   source                            = "../modules/role_assignments"
+//   scope                             = module.azurerm_storage_account_2.id
+//   role_definition_name              = "Storage Blob Data Owner"
+//   principal_id                      = module.azurerm_linux_function_app.principal_id
+// }
 
-module "azurerm_service_plan" {  
-  providers                           =  {azurerm = azurerm.integ-nprod-001}
-  source                              = "../modules/app-service-plan"
-  resource_group_name                 = local.resource_group_name
-  service_plan_name                   = local.asp_service_plan_name
-  location                            = var.location
-  os_type                             = var.os_type
-  aspsku_name                         = var.aspsku_name  
-  worker_count                        = var.worker_count
-  tags                                = var.tags
-  depends_on = [module.resource_group]
-}
+// module "azurerm_service_plan" {  
+//   providers                           =  {azurerm = azurerm.integ-nprod-001}
+//   source                              = "../modules/app-service-plan"
+//   resource_group_name                 = local.resource_group_name
+//   service_plan_name                   = local.asp_service_plan_name
+//   location                            = var.location
+//   os_type                             = var.os_type
+//   aspsku_name                         = var.aspsku_name  
+//   worker_count                        = var.worker_count
+//   tags                                = var.tags
+//   depends_on = [module.resource_group]
+// }
 
 module "azurerm_storage_account_1" {  
   providers                             =  {azurerm = azurerm.integ-nprod-001}
@@ -94,22 +94,22 @@ module "azurerm_storage_account_1" {
   depends_on = [module.resource_group]
 }
 
-module "azurerm_storage_account_2" {  
-  providers                             =  {azurerm = azurerm.integ-nprod-001}
-  source                                = "../modules/storage-account"
-  storage_account_name                  = local.storage_account_name_2
-  resource_group_name                   = local.resource_group_name
-  location                              = var.location
-  account_tier                          = var.account_tier
-  account_replication_type              = var.account_replication_type
-  public_network_access_enabled         = false
-  https_traffic_only_enabled            = true
-  identity_type                         = "SystemAssigned"
-  identity_ids                          = []
-  advanced_threat_protection_enabled    = false
-  tags                                  = var.tags
-  depends_on = [module.resource_group]
-}
+// module "azurerm_storage_account_2" {  
+//   providers                             =  {azurerm = azurerm.integ-nprod-001}
+//   source                                = "../modules/storage-account"
+//   storage_account_name                  = local.storage_account_name_2
+//   resource_group_name                   = local.resource_group_name
+//   location                              = var.location
+//   account_tier                          = var.account_tier
+//   account_replication_type              = var.account_replication_type
+//   public_network_access_enabled         = false
+//   https_traffic_only_enabled            = true
+//   identity_type                         = "SystemAssigned"
+//   identity_ids                          = []
+//   advanced_threat_protection_enabled    = false
+//   tags                                  = var.tags
+//   depends_on = [module.resource_group]
+// }
 
 module "private_endpoint_storage_1" {  
 providers                             =  {azurerm = azurerm.integ-nprod-001}
@@ -127,21 +127,21 @@ private_dns_zone_ids                  = [data.azurerm_private_dns_zone.storageac
 depends_on                            = [module.azurerm_storage_account_1,module.resource_group]
 }
 
-module "private_endpoint_storage_2" {  
-  providers                          =  {azurerm = azurerm.integ-nprod-001}
-  source                             = "../modules/private-endpoint"
-  private_endpoint_name              = "pe-${local.storage_account_name_2}"
-  location                           = var.location
-  resource_group_name                = local.resource_group_name
-  subnet_id                          = data.azurerm_subnet.default_subnet.id
-  private_service_connection_name    = "${local.storage_account_name_2}-psc"
-  private_connection_resource_id     = module.azurerm_storage_account_2.id
-  subresource_names                  = ["blob"]
-  is_manual_connection               = false
-  private_dns_zone_group_name        = "private-dns-zone-group"
-  private_dns_zone_ids               = [data.azurerm_private_dns_zone.storageaccount2_dns.id]
-  depends_on                         = [module.azurerm_storage_account_2,module.resource_group]
-}
+// module "private_endpoint_storage_2" {  
+//   providers                          =  {azurerm = azurerm.integ-nprod-001}
+//   source                             = "../modules/private-endpoint"
+//   private_endpoint_name              = "pe-${local.storage_account_name_2}"
+//   location                           = var.location
+//   resource_group_name                = local.resource_group_name
+//   subnet_id                          = data.azurerm_subnet.default_subnet.id
+//   private_service_connection_name    = "${local.storage_account_name_2}-psc"
+//   private_connection_resource_id     = module.azurerm_storage_account_2.id
+//   subresource_names                  = ["blob"]
+//   is_manual_connection               = false
+//   private_dns_zone_group_name        = "private-dns-zone-group"
+//   private_dns_zone_ids               = [data.azurerm_private_dns_zone.storageaccount2_dns.id]
+//   depends_on                         = [module.azurerm_storage_account_2,module.resource_group]
+// }
 
 module "servicebus" {
   providers                         =  {    azurerm = azurerm.integ-nprod-001}
@@ -217,37 +217,37 @@ module "private_endpoint_key_vault" {
 }
 
 
-module "api_management" {
-  providers                         =  {azurerm = azurerm.integ-nprod-001}
-  source                          = "../modules/api-management"
-  api_management_name             = local.api_management_name
-  location                        = var.location
-  resource_group_name             = local.resource_group_name
-  publisher_name                  = var.publisher_name
-  publisher_email                 = var.publisher_email
-  sku                             = var.api_sku
-  sku_count                       = var.sku_count
-  identity_type                   = "SystemAssigned"
-  identity_ids                    = []
-  tags                            = var.tags
-  depends_on = [module.resource_group]
-}
+// module "api_management" {
+//   providers                         =  {azurerm = azurerm.integ-nprod-001}
+//   source                          = "../modules/api-management"
+//   api_management_name             = local.api_management_name
+//   location                        = var.location
+//   resource_group_name             = local.resource_group_name
+//   publisher_name                  = var.publisher_name
+//   publisher_email                 = var.publisher_email
+//   sku                             = var.api_sku
+//   sku_count                       = var.sku_count
+//   identity_type                   = "SystemAssigned"
+//   identity_ids                    = []
+//   tags                            = var.tags
+//   depends_on = [module.resource_group]
+// }
 
-module "private_endpoint_api_management" {
-  providers                         =  {azurerm = azurerm.integ-nprod-001}
-  source                          = "../modules/private-endpoint"
-  private_endpoint_name           = "pe-${local.api_management_name}"
-  location                        = var.location
-  resource_group_name             = local.resource_group_name
-  subnet_id                       = data.azurerm_subnet.default_subnet.id
-  private_service_connection_name = "${local.api_management_name}-psc"
-  private_connection_resource_id  = module.api_management.id
-  subresource_names               = ["Gateway"]
-  is_manual_connection            = false
-  private_dns_zone_group_name     = "private-dns-zone-group"
-  private_dns_zone_ids            = [data.azurerm_private_dns_zone.api_management_dns.id]  
-  depends_on                      = [module.api_management,module.resource_group]
-}
+// module "private_endpoint_api_management" {
+//   providers                         =  {azurerm = azurerm.integ-nprod-001}
+//   source                          = "../modules/private-endpoint"
+//   private_endpoint_name           = "pe-${local.api_management_name}"
+//   location                        = var.location
+//   resource_group_name             = local.resource_group_name
+//   subnet_id                       = data.azurerm_subnet.default_subnet.id
+//   private_service_connection_name = "${local.api_management_name}-psc"
+//   private_connection_resource_id  = module.api_management.id
+//   subresource_names               = ["Gateway"]
+//   is_manual_connection            = false
+//   private_dns_zone_group_name     = "private-dns-zone-group"
+//   private_dns_zone_ids            = [data.azurerm_private_dns_zone.api_management_dns.id]  
+//   depends_on                      = [module.api_management,module.resource_group]
+// }
 
 // module "app_service_environment" {
 //   providers                         =  {azurerm = azurerm.integ-nprod-001}
