fb9fcab0fe0f7e6ecaa061abee3c1537285d4a6a
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-15 09:50:19 +0000

    Updated locals.tf

--- iac-fortrea/project/locals.tf
+++ iac-fortrea/project/locals.tf
@@ -5,19 +5,21 @@ locals {
 
   logic_app_name               = "la-${local.app_name}-${local.environment}-${local.location_test}-001" 
   app_service_plan_name        = "asp-${local.app_name}-${local.environment}-${local.location_test}-002" 
-  ase_name                     = "ase-${local.app_name}-${local.environment}-${local.location_test}-001"
+  // ase_name                     = "ase-${local.app_name}-${local.environment}-${local.location_test}-001"
 
-  api_management_name          = "api-${local.app_name}-${local.environment}-${local.location_test}-001"
+  // api_management_name          = "api-${local.app_name}-${local.environment}-${local.location_test}-001"
 
-  ase_resource_group_name      = "rg-${local.app_name}-${local.environment}-${local.location_test}-002" 
+  // ase_resource_group_name      = "rg-${local.app_name}-${local.environment}-${local.location_test}-002" 
   storage_resource_group_name  = "rg-${local.app_name}-${local.environment}-${local.location_test}-002"
 
 
   function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location_test}-001"  
   resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-002"
   asp_service_plan_name   = "asp-${local.app_name}-${local.environment}-${local.location_test}-001"
-  servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location_test}-001"
+  // servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location_test}-001"
   key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location_test}-001"
   storage_account_name_1  = "stg${local.app_name}${local.environment}${local.location_test}001"
   storage_account_name_2  = "stg${local.app_name}${local.environment}${local.location_test}002"
+  monitor_diagnostic_name = "mds-${local.app_name}-${local.environment}-${local.location_test}-001"
+  monitor_diagnostic_name = "mds-${local.app_name}-${local.environment}-${local.location_test}-002"
 }
