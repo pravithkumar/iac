16a5c9a8c3e8d7c6fb225311cdb1da548e6c307f
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-15 11:06:15 +0000

    Updated locals.tf

--- iac-fortrea/project/locals.tf
+++ iac-fortrea/project/locals.tf
@@ -7,7 +7,7 @@ locals {
   app_service_plan_name        = "asp-${local.app_name}-${local.environment}-${local.location_test}-002" 
   // ase_name                     = "ase-${local.app_name}-${local.environment}-${local.location_test}-001"
 
-  // api_management_name          = "api-${local.app_name}-${local.environment}-${local.location_test}-001"
+  api_management_name          = "api-${local.app_name}-${local.environment}-${local.location_test}-001"
 
   // ase_resource_group_name      = "rg-${local.app_name}-${local.environment}-${local.location_test}-002" 
   storage_resource_group_name  = "rg-${local.app_name}-${local.environment}-${local.location_test}-002"
@@ -16,7 +16,7 @@ locals {
   function_app_name       = "fa-${local.app_name}-${local.environment}-${local.location_test}-001"  
   resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-002"
   asp_service_plan_name   = "asp-${local.app_name}-${local.environment}-${local.location_test}-001"
-  // servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location_test}-001"
+  servicebus_name         = "sb-${local.app_name}-${local.environment}-${local.location_test}-001"
   key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location_test}-001"
   storage_account_name_1  = "stg${local.app_name}${local.environment}${local.location_test}001"
   storage_account_name_2  = "stg${local.app_name}${local.environment}${local.location_test}002"
