4e9e9e3ce025a16adfa17d1de21e4102d90d10eb
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-15 09:42:57 +0000

    Updated output.tf

--- modules/logic-app/output.tf
+++ modules/logic-app/output.tf
@@ -2,3 +2,7 @@ output "id" {
   value = azurerm_logic_app_standard.logic_app.id
   description = "The ID of the Logic App Standard"
 }
+
+output "principal_id" {
+  value = azurerm_logic_app_standard.logic_app.identity[0].principal_id
+}
