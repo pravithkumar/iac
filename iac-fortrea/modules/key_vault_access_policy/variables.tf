cb5cf3c7ac6013364ce6e5c149e1ba95c4d40d6f
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-16 12:40:39 +0000

    Updated variables.tf

--- iac-fortrea/modules/key_vault_access_policy/variables.tf
+++ iac-fortrea/modules/key_vault_access_policy/variables.tf
@@ -1,20 +1,20 @@
 variable "key_vault_id" {
   description = "The ID of the Key Vault"
-  type        = string
+  type = string
 }
 
 variable "tenant_id" {
   description = "The tenant ID"
-  type        = string
+  type = string
 }
 
 variable "object_id" {
   description = "The object ID of the principal"
-  type        = string
+  type = string
 }
 
 variable "secret_permissions" {
   description = "List of secret permissions"
-  type        = list(string)
-  default     = ["get", "list"]
+  type = list(string)
+  default = ["get", "list"]
 }
