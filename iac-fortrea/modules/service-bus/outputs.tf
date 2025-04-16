48b87276ca819935ffa9073876bc1a23440c86a0
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-16 08:01:57 +0000

    Updated outputs.tf

--- iac-fortrea/modules/service-bus/outputs.tf
+++ iac-fortrea/modules/service-bus/outputs.tf
@@ -1,3 +1,7 @@
 output "servicebus_id" {
   value = azurerm_servicebus_namespace.servicebus.id 
 }
+
+output "principal_id" {
+  value = azurerm_servicebus_namespace.servicebus.identity[0].principal_id
+}
