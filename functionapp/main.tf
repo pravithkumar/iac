477b9b1a7a747bd35aadbe70c0d43e86b0ad3716
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-17 12:12:08 +0000

    Updated main.tf

--- functionapp/main.tf
+++ functionapp/main.tf
@@ -25,14 +25,11 @@ module "azurerm_linux_function_app" {
   app_settings                        = var.app_settings
   enable_app_insights                 = true
   appinsights_instrumentationkey      = data.azurerm_application_insights.ai.instrumentation_key
-  applicationinsights_connectionstring = data.azurerm_application_insights.ai.connection_string
-
-  
-  auth_enabled                        = var.auth_enabled
-  allowed_external_redirect_urls      = var.allowed_external_redirect_urls
-  client_id                           = var.client_id
-  tenant_auth_endpoint                = var.tenant_auth_endpoint
-
+  applicationinsights_connectionstring = data.azurerm_application_insights.ai.connection_string  
+  auth_enabled                         = var.auth_enabled
+  allowed_external_redirect_urls       = var.allowed_external_redirect_urls
+  client_id                            = var.client_id
+  tenant_auth_endpoint                 = var.tenant_auth_endpoint
   depends_on                          = [module.azurerm_service_plan, module.azurerm_storage_account_1,module.resource_group]
 }
 
