ccc770c06936ec45ca305d14f45715bf59e2fade
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-18 10:59:09 +0000

    Updated main.tf

--- iac-fortrea/modules/api-management/main.tf
+++ iac-fortrea/modules/api-management/main.tf
@@ -15,3 +15,13 @@ resource "azurerm_api_management" "api" {
   }
 }
 }
+resource "azurerm_api_management_logger" "logger" {
+  name                = "$(var.api_management_name)-logger"
+  api_management_name = azurerm_api_management.api.name
+  resource_group_name = azurerm_resource_group.api.name
+  resource_id         = var.app_insights_id
+
+  application_insights {
+    instrumentation_key = var.app_insights_key
+  }
+}
