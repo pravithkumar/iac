3ea443f0360a8a8627c7f37d80cc5df5ad9f9d18
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-18 11:36:01 +0000

    Updated main.tf

--- iac-fortrea/modules/api-management/main.tf
+++ iac-fortrea/modules/api-management/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_api_management" "api" {
 resource "azurerm_api_management_logger" "logger" {
   name                = "$(var.api_management_name)-logger"
   api_management_name = azurerm_api_management.api.name
-  resource_group_name = azurerm_resource_group.api.name
+  resource_group_name = var.resource_group_name
   resource_id         = var.app_insights_id
 
   application_insights {
