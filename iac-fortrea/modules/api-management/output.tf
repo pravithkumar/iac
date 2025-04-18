0c6675c2749f87ae18849f36db79d9fef7768f9a
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-18 07:58:34 +0000

    Updated output.tf

--- iac-fortrea/modules/api-management/output.tf
+++ iac-fortrea/modules/api-management/output.tf
@@ -2,3 +2,7 @@ output "id" {
   description = "The ID of the API Management Service."
   value       = azurerm_api_management.api.id
 }
+
+output "principal_id" {
+  value = azurerm_api_management.api.identity[0].principal_id
+}
