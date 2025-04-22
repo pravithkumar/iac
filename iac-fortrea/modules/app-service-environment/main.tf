10022f5640ac24ca0e48e12a146910b2a5b57400
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-22 08:20:36 +0000

    Updated main.tf

--- iac-fortrea/modules/app-service-environment/main.tf
+++ iac-fortrea/modules/app-service-environment/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_app_service_environment_v3" "ase" {
   dynamic "cluster_setting" {
     for_each = var.cluster_settings
     content {
-      name  = cluster_setting.key
+      name  = cluster_setting.key
       value = cluster_setting.value
       }
   }
