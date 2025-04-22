a23ed0a275ce71e3ba6c40b221034bb30f60d47c
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-22 08:19:04 +0000

    Updated main.tf

--- iac-fortrea/modules/app-service-environment/main.tf
+++ iac-fortrea/modules/app-service-environment/main.tf
@@ -5,11 +5,11 @@ resource "azurerm_app_service_environment_v3" "ase" {
   internal_load_balancing_mode  = var.internal_load_balancing_mode
   
   dynamic "cluster_setting" {
-   for_each = var.cluster_settings
-    content {
-      name  = cluster_setting.key
-      value = cluster_setting.value
-    }
-  }
+    for_each = var.cluster_settings
+    content {
+      name  = cluster_setting.key
+      value = cluster_setting.value
+      }
+  }
   tags = var.tags
 }
