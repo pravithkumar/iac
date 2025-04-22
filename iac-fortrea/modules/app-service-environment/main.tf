086a9479e333db88cf828ac9fc14f50a5556dc75
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-22 13:09:45 +0000

    Updated main.tf

--- iac-fortrea/modules/app-service-environment/main.tf
+++ iac-fortrea/modules/app-service-environment/main.tf
@@ -3,13 +3,43 @@ resource "azurerm_app_service_environment_v3" "ase" {
   resource_group_name           = var.resource_group_name
   subnet_id                     = var.subnet_id
   internal_load_balancing_mode  = var.internal_load_balancing_mode
-  
-  dynamic "cluster_setting" {
-  for_each = var.cluster_settings
-  content {
-    name  = cluster_setting.key
-    value = cluster_setting.value
+  timeouts {
+        create = "120m"
+        delete = "120m"
+      }
+  cluster_setting {
+    name  = "DisableTls1.0"
+    value = var.disable_tls1_0
   }
+  cluster_setting {
+    name  = "InternalEncryption"
+    value = var.internal_encryption
+  }
+  cluster_setting {
+    name  = "FrontEndSSLCipherSuiteOrder"
+    value = var.frontend_ssl_cipher_suite_order
+  }
+  
+  cluster_setting {
+  name = "RemoteDebugEnabled"
+  value = false
 }
+cluster_setting {
+  name = "FtpEnabled"
+  value = false
+}
+cluster_setting {
+  name = "AllowNewPrivateEndpointConnections"
+  value = true
+}
+cluster_setting {
+  name = "MaxWorkerCount"
+  value = 10
+}
+cluster_setting {
+  name = "DiagnosticLogsEnabled"
+  value = true
+}
+
   tags = var.tags
 }
