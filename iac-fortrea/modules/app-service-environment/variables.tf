7a5f589e26eb5f42a4b2ab6412ba62d2b9703efd
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-22 13:53:43 +0000

    Updated variables.tf

--- iac-fortrea/modules/app-service-environment/variables.tf
+++ iac-fortrea/modules/app-service-environment/variables.tf
@@ -45,3 +45,15 @@ variable "tags" {
     terraformed = "true"
   }
 }
+
+variable "identity_type" {
+  description = "The type of identity to assign to the resource. Possible values are 'SystemAssigned' or 'UserAssigned'."
+  type        = string
+  default     = "SystemAssigned"
+}
+
+variable "identity_ids" {
+  description = "The list of user-assigned identity IDs to assign to the resource. Only used if identity_type is 'UserAssigned'."
+  type        = list(string)
+  default     = []
+}
