9a285846018c2eb82abf52ec46a4da344e4af11b
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-04-22 05:27:27 +0000

    Updated main.tf

--- iac-fortrea/modules/function-app/main.tf
+++ iac-fortrea/modules/function-app/main.tf
@@ -13,10 +13,11 @@ resource "azurerm_linux_function_app" "fa" {
   location                   = var.location
   resource_group_name        = var.resource_group_name
   service_plan_id            = data.azurerm_service_plan.asp.id
-  storage_account_name       = data.azurerm_storage_account.sa.name
-  storage_account_access_key = data.azurerm_storage_account.sa.primary_access_key
+  storage_account_name       = data.azurerm_storage_account.sa.name  
   https_only                 = var.https_only
+  public_network_access_enabled = false
   tags                       = var.tags
+  virtual_network_subnet_id  = var.subnet_id
   dynamic "identity" {
   for_each = var.identity_type != null ? [1] : []
   content {
@@ -25,7 +26,18 @@ resource "azurerm_linux_function_app" "fa" {
   }
 }
   site_config {
-    always_on = var.always_on
+    always_on = var.always_on   
+ 
+    dynamic "application_stack" {
+      for_each = var.runtime != null ? [1] : []
+      content {       
+        dotnet_version = var.runtime == "dotnet" ? var.runtime_version : null       
+        java_version = var.runtime == "java" ? var.runtime_version : null
+        node_version = var.runtime == "node" ? var.runtime_version : null
+        python_version = var.runtime == "python" ? var.runtime_version : null
+        powershell_core_version = var.runtime == "powershell" ? var.runtime_version : null      
+      }
+    }
   }
   app_settings = merge(var.enable_app_insights ? {
       APPINSIGHTS_INSTRUMENTATIONKEY = var.appinsights_instrumentationkey
@@ -33,10 +45,20 @@ resource "azurerm_linux_function_app" "fa" {
     } : {},
     var.app_settings,
   )
-}
+ 
+auth_settings_v2 {
+  auth_enabled    = var.auth_enabled
 
-resource "azurerm_role_assignment" "function_app_role" {
-  scope                = data.azurerm_storage_account.sa.id 
-  role_definition_name = "Storage Blob Data Contributor"                               # Or use role_definition_id
-  principal_id         = azurerm_linux_function_app.fa.identity[0].principal_id             # Replace with the principal's object ID
+  login {
+    allowed_external_redirect_urls = var.allowed_external_redirect_urls
+  }
+
+  dynamic "active_directory_v2" {
+    for_each = var.auth_enabled ? [1] : []
+    content {
+      client_id           = var.client_id
+      tenant_auth_endpoint = var.tenant_auth_endpoint 
+    }
+  }
+ }
 }
