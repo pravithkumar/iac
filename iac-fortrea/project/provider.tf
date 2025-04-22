825205f8a0a7fe62f6a6341680dbfa8c55634903
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-22 10:52:19 +0000

    Updated provider.tf

--- iac-fortrea/project/provider.tf
+++ iac-fortrea/project/provider.tf
@@ -19,14 +19,14 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "c6806c08-6845-4be9-bda8-0d35ff0251eb"
+  subscription_id = "795783af-96d3-4629-9161-58de5577ed1e"
   features {}
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   alias           = "integ-nprod-001"
-  subscription_id = "c6806c08-6845-4be9-bda8-0d35ff0251eb"
+  subscription_id = "795783af-96d3-4629-9161-58de5577ed1e"
   features {}
 }
 
