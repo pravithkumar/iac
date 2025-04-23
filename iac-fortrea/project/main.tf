1e56179cde7fee1a62e672c2f7ef197f36e07b6b
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-23 10:15:53 +0000

    Updated main.tf

--- iac-fortrea/project/main.tf
+++ iac-fortrea/project/main.tf
@@ -288,7 +288,7 @@ module "app_logic_app" {
   enable_app_insights                 = true
   appinsights_instrumentationkey      = data.azurerm_application_insights.ai.instrumentation_key
   applicationinsights_connectionstring = data.azurerm_application_insights.ai.connection_string 
-  depends_on                      = [module.azurerm_storage_account_1,module.resource_group]
+  depends_on                      = [module.app_service_environment,module.azurerm_storage_account_1,module.resource_group]
 }
 
 module "azurerm_storage_account_1" {  
