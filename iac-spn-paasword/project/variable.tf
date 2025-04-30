4fb90ed90f8dcc776265f5917bbdf2293e0906a0
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 09:14:13 +0000

    Updated variable.tf

--- iac-spn-paasword/project/variable.tf
+++ iac-spn-paasword/project/variable.tf
@@ -12,3 +12,10 @@ variable "resource_group_name" {
   description = "The name of the resource group containing the Key Vault"
   type        = string
 }
+
+variable "location_test" {
+  type        = string
+  description = "Location for testing"
+  default     = "eastus"
+}
+
