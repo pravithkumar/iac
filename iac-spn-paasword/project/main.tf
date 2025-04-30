6f05e8cd48840c69d74cd84b25d32d893bbc36eb
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 10:28:53 +0000

    Updated main.tf

--- iac-spn-paasword/project/main.tf
+++ iac-spn-paasword/project/main.tf
@@ -1,6 +1,5 @@
 module "spn_password" {
   source = "../modules/spn_password"
-  service_principal_id = var.service_principal_id
   keyvault_name        = var.keyvault_name
   resource_group_name  = var.resource_group_name  
 }
