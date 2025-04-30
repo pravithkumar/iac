c3e2266b8d7cde4a3ae25d4e99a9069b84638170
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 14:59:06 +0000

    Updated output.tf

--- iac-spn-paasword/modules/spn_password/output.tf
+++ iac-spn-paasword/modules/spn_password/output.tf
@@ -1,8 +1,3 @@
-output "spn_password_value" {
-  description = "The generated service principal password"
-  value       = random_password.spn_password.result
-}
-
 output "key_vault_secret_id" {
   description = "The ID of the Key Vault secret"
   value       = azurerm_key_vault_secret.example.id
