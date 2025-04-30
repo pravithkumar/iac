1102eec0f5f3b3f7f5bc0e2577a3f63dcaba9d3f
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 14:43:05 +0000

    Updated main.tf

--- iac-spn-paasword/modules/spn_password/main.tf
+++ iac-spn-paasword/modules/spn_password/main.tf
@@ -21,7 +21,7 @@ data "azurerm_key_vault" "existing" {
 
 resource "azurerm_key_vault_secret" "example" {
   name         = "spn-password"
-  value        = azuread_service_principal_password.example.value
+  value        = azuread_application_password.example.value
   key_vault_id = data.azurerm_key_vault.existing.id
 }
 
