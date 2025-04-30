a89fc9e2e28aaf53ee6970620cb723c72da79335
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 11:51:47 +0000

    Updated main.tf

--- iac-spn-paasword/project/main.tf
+++ iac-spn-paasword/project/main.tf
@@ -1,6 +1,6 @@
 module "spn_password" {
-  source = "../modules/spn_password"
   providers            =  {azurerm = azurerm.integ-nprod-001}
+  source = "../modules/spn_password"
   keyvault_name        = var.keyvault_name
   resource_group_name  = var.resource_group_name  
 }
