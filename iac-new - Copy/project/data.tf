fe223e7b8958b0c783ba6be9fc1c6cbff6a0aec8
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 07:52:17 +0000

    Updated data.tf

---
+++ iac-new - Copy/project/data.tf
@@ -0,0 +1,8 @@
+data "azurerm_key_vault" "existing" {
+  name                = var.keyvault_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azuread_service_principal" "example" {
+  object_id = var.service_principal_id
+}
