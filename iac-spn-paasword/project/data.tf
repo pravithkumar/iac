2c30c4c1548a0f3719b7d502733f50acb8b867ee
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 10:35:37 +0000

    Updated data.tf

--- iac-spn-paasword/project/data.tf
+++ iac-spn-paasword/project/data.tf
@@ -3,6 +3,6 @@ data "azurerm_key_vault" "existing" {
   resource_group_name = var.resource_group_name
 }
 
-data "azuread_service_principal" "example" {
-  object_id = var.service_principal_id
-}
+// data "azuread_service_principal" "example" {
+//   object_id = var.service_principal_id
+// }
