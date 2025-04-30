ebc329928ffbdaaaf2672484a553b4af7720046b
Pravith.Yedla@cogcis.onmicrosoft.com <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-04-30 13:13:17 +0530

    changes

---
+++ iac-new - Copy/modules/api-management/main.tf
@@ -0,0 +1,21 @@
+resource "random_password" "spn_password" {
+  length  = 16
+  special = true
+}
+
+resource "azuread_service_principal_password" "example" {
+  service_principal_id = var.service_principal_id
+  value                = random_password.spn_password.result
+  end_date_relative    = "2160h"  # Valid for 90 days
+}
+
+data "azurerm_key_vault" "existing" {
+  name                = var.keyvault_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "spn-password"
+  value        = random_password.spn_password.result
+  key_vault_id = data.azurerm_key_vault.existing.id
+}
---
+++ iac-new - Copy/project/parameters/dev-terrafrom.tfvars
@@ -0,0 +1,3 @@
+﻿service_principal_id = "<your-service-principal-id>"
+keyvault_name        = "<your-existing-keyvault-name>"
+resource_group_name  = "<your-resource-group-name>"
