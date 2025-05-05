2c34aaffc9764dbb4683b742f9ac26f4048c09d3
PavanKumar.G <PavanKumar.G@cogcis.onmicrosoft.com>
2025-05-05 06:55:53 +0000

    Updated main.tf

--- iac-spn-paasword/modules/spn_password/main.tf
+++ iac-spn-paasword/modules/spn_password/main.tf
@@ -3,7 +3,7 @@ data "azuread_application" "example" {
 }
 
 resource "azuread_application_password" "this" {  
-  application_id = data.azuread_application.example.application_id
+  application_id = data.azuread_application.example.object_id
   display_name   = var.app_password_display_name
   end_date       = timeadd(timestamp(), "2160h") # Valid for 90 days
 }
