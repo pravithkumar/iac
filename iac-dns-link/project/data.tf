02fe03de3ac4a2a424b3a5355689104cabfd995b
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-05-06 06:37:21 +0000

    Updated data.tf

--- iac-dns-link/project/data.tf
+++ iac-dns-link/project/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_virtual_network" "example" {
-  provider           = azurerm.integ-nprod-001
-  name                = "vnet-int-dev-esu-001"
-  resource_group_name = "rg-int-vnet-esu-001"
+  provider           = azurerm.integ-mgmt-001
+  name                = "vnet-int-esu-002"
+  resource_group_name = "rg-int-vnet-esu-003"
 }
