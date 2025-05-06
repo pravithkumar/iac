96e37b74536a9e9496bef209bda97db305bf10bf
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-05-06 05:52:15 +0000

    Updated main.tf

--- iac-dns-link/project/main.tf
+++ iac-dns-link/project/main.tf
@@ -2,7 +2,7 @@ module "dns_zone_links" {
   providers            = { azurerm = integ-nprod-001 }
   source               = "../modules/dns_link"
   resource_group_name  = var.resource_group_name
-  vnet_id              = data.azurerm_virtual_network.example.id
+  virtual_network_id   = data.azurerm_virtual_network.example.id
   dns_zones            = var.dns_zones
 }
 
