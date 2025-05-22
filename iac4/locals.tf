d8ccfe85dec876877f6f4a4ca1fffe3f2bdf702e
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-05-22 07:53:53 +0000

    Deleted iac4

--- iac4/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  app_name     = var.app_name
-  environment  = var.environment
-  location_test = var.location_test
-
-  resource_group_name     = "rg-${local.app_name}-${local.environment}-${local.location_test}-004"
-  key_vault_name          = "kv-${local.app_name}-${local.environment}-${local.location_test}-004"
-
-}
