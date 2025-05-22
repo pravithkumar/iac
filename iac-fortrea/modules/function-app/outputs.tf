149c2f8c8b58231993a06490a4b27750516afb15
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-05-22 07:37:51 +0000

    Deleted iac-fortrea

--- iac-fortrea/modules/function-app/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "function_app_id" {
-  value = azurerm_linux_function_app.fa.id
-}
-
-output "principal_id" {
-  value = azurerm_linux_function_app.fa.identity[0].principal_id
-}
--- iac-fortrea/modules/service-bus/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "servicebus_id" {
-  value = azurerm_servicebus_namespace.servicebus.id 
-}
-
-output "principal_id" {
-  value = azurerm_servicebus_namespace.servicebus.identity[0].principal_id
-}
