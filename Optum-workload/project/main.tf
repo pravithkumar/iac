83d35184973e28e062e789093a07ecf18c3aa7d8
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-03 12:46:49 +0000

    Updated main.tf

--- Optum-workload/project/main.tf
+++ Optum-workload/project/main.tf
@@ -1,12 +1,19 @@
 module "avd_deployment" {
   providers            =  {azurerm = azurerm.integ-nprod-001}
-  source = "../modules/avd_deployment"
-
+  source = "./modules/avd_deployment" 
+  existing_resource_group_name = var.existing_resource_group_name
+  existing_virtual_network_name = var.existing_virtual_network_name
+  existing_subnet_name       = var.existing_subnet_name
+  location                   = var.location
+  host_pool_name             = var.host_pool_name
+  host_pool_type             = var.host_pool_type
+  load_balancer_type         = var.load_balancer_type
+  max_session_hosts          = var.max_session_hosts
+  default_desktop_app_group_name = var.application_group_name
+  workspace_name             = var.workspace_name
+  vm_size                    = var.vm_size
+  admin_username             = var.admin_username
   
-  keyvault_name        = var.keyvault_name
-  resource_group_name  = var.resource_group_name
-  app_password_display_name =var.app_password_display_name
-  app_kv_secret_name        =var.app_kv_secret_name
-  spn_name                  =var.spn_name
-  password_validity_days    =var.password_validity_days
+  
 }
+
