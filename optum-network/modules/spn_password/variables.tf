fe8f8df7229cabe66d6f53ea028921aeb3ba9a77
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-06-03 07:41:49 +0000

    Updated variables.tf

--- optum-network/modules/spn_password/variables.tf
+++ optum-network/modules/spn_password/variables.tf
@@ -1,29 +1,16 @@
-variable "keyvault_name" {
-  description = "The name of the existing Key Vault"
-  type        = string
+variable "resource_group_name" {}
+variable "location" {}
+
+variable "vnet_name" {}
+variable "vnet_address_space" {
+  type = list(string)
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group containing the Key Vault"
-  type        = string
-}
-variable "app_password_display_name" {
-  type        = string
-  description = "application password display name" 
-}
-
-variable "app_kv_secret_name" {
-  type        = string
-  description = "application keyvault secret name" 
-}
-
-variable "spn_name" {
-  type        = string
-  description = "SPN name" 
-}
-
-variable "password_validity_days" {
-  description = "Number of days the password is valid for"
-  type        = number
-  default     = 90
+variable "subnets" {
+  type = list(object({
+    name             = string
+    address_prefixes = list(string)
+    nsg_id           = optional(string)
+    route_table_id   = optional(string)
+  }))
 }
