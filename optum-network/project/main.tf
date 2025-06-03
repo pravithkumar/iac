e62c7add116804f3294fbd2b9aea239e07a72027
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-06-03 08:42:39 +0000

    Updated main.tf

--- optum-network/project/main.tf
+++ optum-network/project/main.tf
@@ -1,10 +1,8 @@
-module "spn_password" {
-  providers            =  {azurerm = azurerm.integ-nprod-001}
-  source = "../modules/spn_password"
-  keyvault_name        = var.keyvault_name
-  resource_group_name  = var.resource_group_name
-  app_password_display_name =var.app_password_display_name
-  app_kv_secret_name        =var.app_kv_secret_name
-  spn_name                  =var.spn_name
-  password_validity_days    =var.password_validity_days
+module "networking" {
+  source              = "./modules/networking"  # Adjust path as needed
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
+  subnets             = var.subnets
 }
