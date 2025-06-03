9f7bf62ebb79552622edfdf7b42ce01d7b1020a2
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-03 09:52:35 +0000

    Updated main.tf

--- Optum-workload/modules/avd_deployment/main.tf
+++ Optum-workload/modules/avd_deployment/main.tf
@@ -1,15 +1,46 @@
 data "azurerm_resource_group" "rg" {
-  name = "optum-chinmayee"
-}
+  name = var.existing_resource_group_name
+  location = var.location
+  }
 
 data "azurerm_virtual_network" "vnet" {
-  name      = "chinmayeevnet"
+  name      = var.existing_virtual_network_name
   resource_group_name = "data.azurerm_resource_group.rg.name"
 }
 
 data "azurerm_subnet" "subnet" {
-  name    = "chinmayeesubnet"
+  name    = var.existing_subnet_name
   virtual_network_name    = "data.azurerm_virtual_network.vnet.name"
   resource_group_name = "data.azurerm_resource_group.rg.name"
-
 }
+
+#-----AVD workspace (Desktop)-------#
+
+resource "azurerm_virtual_desktop_workspace" "avd_workspace" {
+  name                = var.workspace_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+}
+
+#------AVD Host Pool -----#
+
+resource "azurerm_virtual_desktop_host_pool" "avd-host-pool" {
+  name                      = var.host_pool_name
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  location                  = data.azurerm_resource_group.rg.location
+  type                      = var.host_pool_type
+  load_balancer_type        = var.load_balancer_type
+  maximum_sessions_allowed  = var.max_session_hosts
+  description               = "AVD Host pool with no session host config"
+}
+
+
+
+
+
+
+
+
+
+
+
