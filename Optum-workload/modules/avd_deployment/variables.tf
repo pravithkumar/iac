b358c05a39f135110ec307ce994bc439a224cfd8
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-03 09:52:46 +0000

    Updated variables.tf

--- Optum-workload/modules/avd_deployment/variables.tf
+++ Optum-workload/modules/avd_deployment/variables.tf
@@ -25,3 +25,37 @@ variable "location" {
   type        = string
   default     = "East US" 
 }
+
+variable "workspace_name" {
+  description = "Name of AVD workspace"
+  type        = string
+  default     = "avd-workspace"
+}
+
+variable "host_pool_name" {
+  description   = "Host pool name"
+  type          = string
+  default       = "avd-hostpool"
+}
+variable "host_pool_type" {
+  description   = "Host pool type"
+  type          = string
+  default       = "Pooled"
+}
+variable "load_balancer_type" {
+  description   = "Load Balancer Type"
+  type          = string
+  default       = "Breadth-first"
+}
+variable "max_session_hosts" {
+  description   = "Maximum session hosts allowed"
+  type          = number
+  default       = 3 
+}
+
+
+
+
+
+
+
