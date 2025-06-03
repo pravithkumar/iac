e34cb58b691d857d3b2b3ba84e70df908e953daa
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-03 08:26:03 +0000

    Updated output.tf

--- Optum-workload/modules/avd_deployment/output.tf
+++ Optum-workload/modules/avd_deployment/output.tf
@@ -1,4 +1,14 @@
-output "key_vault_secret_id" {
-  description = "The ID of the Key Vault secret"
-  value       = azurerm_key_vault_secret.this.id
+output "resource_group_name" {
+  description = "The ID of the existing Resource Group."
+  value       = data.azurerm_resource_group.rg.name
+}
+
+output "vnet_id" {
+  description = "The ID of the existing Virtual Network."
+  value       = data.azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+  description = "The ID of the existing Subnet."
+  value       = data.azurerm_subnet.subnet.id
 }
