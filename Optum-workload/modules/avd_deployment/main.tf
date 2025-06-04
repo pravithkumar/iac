6baf1488d52e45c96c39da745f6c9b06d16b12da
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-04 13:27:44 +0000

    Updated main.tf

--- Optum-workload/modules/avd_deployment/main.tf
+++ Optum-workload/modules/avd_deployment/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "data.azurerm_subnet.subnet.id"
+    subnet_id                     = data.azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
