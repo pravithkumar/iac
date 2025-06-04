8a42cc71a784d3652108e6b348c23a67c9d0e083
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-04 09:36:55 +0000

    Updated main.tf

--- Optum-workload/project/main.tf
+++ Optum-workload/project/main.tf
@@ -1,10 +1,6 @@
 module "avd_deployment" {
   providers            =  {azurerm = azurerm.integ-nprod-001}
   source = "../modules/avd_deployment" 
-  existing_resource_group_name = var.existing_resource_group_name
-  existing_virtual_network_name = var.existing_virtual_network_name
-  existing_subnet_name       = var.existing_subnet_name
-  location                   = var.location
   host_pool_name             = var.host_pool_name
   host_pool_type             = var.host_pool_type
   load_balancer_type         = var.load_balancer_type
