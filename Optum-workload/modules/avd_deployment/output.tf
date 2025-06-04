bfc054e1272f59df1f41b7a095ee776a3b04c974
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-04 14:27:48 +0000

    Updated output.tf

--- Optum-workload/modules/avd_deployment/output.tf
+++ Optum-workload/modules/avd_deployment/output.tf
@@ -1,24 +1,24 @@
-output "resource_group_location" {
-  value       = data.azurerm_resource_group.rg.location
-  description = "The location of the existing Azure Resource Group."
-}
+#output "resource_group_location" {
+#value       = data.azurerm_resource_group.rg.location
+# description = "The location of the existing Azure Resource Group."
+#}
 
-output "resource_group_name" {
-  value       = data.azurerm_resource_group.rg.name
-  description = "The name of the existing Azure Resource Group."
-}
+#output "resource_group_name" {
+# value       = data.azurerm_resource_group.rg.name
+# description = "The name of the existing Azure Resource Group."
+#}
 
 
 
-output "host_pool_name" {
-  description = "The name of the deployed AVD Host Pool."
-  value       = azurerm_virtual_desktop_host_pool.avd-host-pool.name
-}
+#output "host_pool_name" {
+# description = "The name of the deployed AVD Host Pool."
+#value       = azurerm_virtual_desktop_host_pool.avd-host-pool.name
+#}
 
-output "workspace_name" {
-  description = "The name of the AVD Workspace."
-  value       = azurerm_virtual_desktop_workspace.avd_workspace.name
-}
+#-output "workspace_name" {
+#description = "The name of the AVD Workspace."
+#value       = azurerm_virtual_desktop_workspace.avd_workspace.name
+#}
 output "workspace_id" {
   description = "The name of the AVD Workspace."
   value       = azurerm_virtual_desktop_workspace_application_group_association.workspace_grp_assc.workspace_id  
