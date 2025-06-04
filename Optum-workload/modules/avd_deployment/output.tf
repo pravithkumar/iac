721cf7fb8a3beb93d200b40444454039c106ebeb
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-04 07:54:47 +0000

    Updated output.tf

--- Optum-workload/modules/avd_deployment/output.tf
+++ Optum-workload/modules/avd_deployment/output.tf
@@ -1,14 +1,14 @@
 output "host_pool_name" {
   description = "The name of the deployed AVD Host Pool."
-  value       = module.avd_deployment.host_pool_name
+  value       = azurerm_virtual_desktop_host_pool.avd-host-pool.host_pool_name
 }
 
 output "workspace_name" {
   description = "The name of the AVD Workspace."
-  value       = module.avd_deployment.workspace_name
+  value       = azurerm_virtual_desktop_workspace.avd_workspace.workspace_name
 }
 
 output "nic_id" {
   description = "The ID of the created Network Interface Card."
-  value       = module.avd_deployment.nic.id
+  value       = azurerm_network_interface.nic.id
 }
