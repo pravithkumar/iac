01382232a847a9c69d174d3625b82fa6dc20bb90
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-06 13:05:32 +0000

    Updated main.tf

--- optum-image-gallery/modules/image_gallery/main.tf
+++ optum-image-gallery/modules/image_gallery/main.tf
@@ -1,20 +1,17 @@
-data "azuread_application" "example" {
-  display_name = var.spn_name
+data "azurerm_resource_group" "rg" {
+  name = var.existing_resource_group_name
+
+  }
+
+data "azurerm_virtual_network" "vnet" {
+  name      = var.existing_virtual_network_name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-resource "azuread_application_password" "this" {  
-  application_id = "/applications/${data.azuread_application.example.object_id}"
-  display_name   = var.app_password_display_name
-  end_date       = timeadd(timestamp(), "${var.password_validity_days * 24}h")
+data "azurerm_subnet" "subnet" {
+  name    = var.existing_subnet_name
+  virtual_network_name    = data.azurerm_virtual_network.vnet.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-data "azurerm_key_vault" "existing" {
-  name                = var.keyvault_name
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_key_vault_secret" "this" {
-  name         = var.app_kv_secret_name
-  value        = azuread_application_password.this.value
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
+#-----------Image Gallery
