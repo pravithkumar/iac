4b35062b61a23f4a8c34f2276a1c755e0b8040f6
Pravith Yedla <Pravith.Yedla@cogcis.onmicrosoft.com>
2025-06-06 08:57:20 +0000

    Updated main.tf

--- optum-nsg-routetable/project/main.tf
+++ optum-nsg-routetable/project/main.tf
@@ -1,10 +1,33 @@
-module "spn_password" {
-  providers            =  {azurerm = azurerm.integ-nprod-001}
-  source = "../modules/spn_password"
-  keyvault_name        = var.keyvault_name
-  resource_group_name  = var.resource_group_name
-  app_password_display_name =var.app_password_display_name
-  app_kv_secret_name        =var.app_kv_secret_name
-  spn_name                  =var.spn_name
-  password_validity_days    =var.password_validity_days
+module "nsg" {
+  source              = "./modules/nsg"
+  name                = "example-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  security_rules = [
+    {
+      name                       = "AllowSSH"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+}
+
+module "route_table" {
+  source              = "./modules/route_table"
+  name                = "example-rt"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  routes = [
+    {
+      name           = "default-route"
+      address_prefix = "0.0.0.0/0"
+      next_hop_type  = "Internet"
+    }
+  ]
 }
