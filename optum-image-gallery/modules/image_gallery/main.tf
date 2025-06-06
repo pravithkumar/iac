f13051ba06875f8e8a9e54b3149f3439290e7251
Musku.Chinmayee <Musku.Chinmayee@cogcis.onmicrosoft.com>
2025-06-06 13:07:33 +0000

    Updated main.tf

--- optum-image-gallery/modules/image_gallery/main.tf
+++ optum-image-gallery/modules/image_gallery/main.tf
@@ -15,3 +15,12 @@ data "azurerm_subnet" "subnet" {
 }
 
 #-----------Image Gallery
+
+
+resource "azurerm_shared_image_gallery" "image_gallery" {
+  name                = var.gallery_name
+  location            = var.location 
+  resource_group_name = azurerm_resource_group.rg.name    
+  description         = var.gallery_description
+
+}
