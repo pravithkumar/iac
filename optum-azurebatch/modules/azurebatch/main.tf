74a352113dea39df39b78d0d10c2421c474524aa
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-02 21:38:42 +0530

    Update main.tf

--- optum-azurebatch/modules/azurebatch/main.tf
+++ optum-azurebatch/modules/azurebatch/main.tf
@@ -11,26 +11,29 @@ resource "azurerm_batch_pool" "pool" {
   resource_group_name = var.resource_group_name
   account_name        = azurerm_batch_account.batch.name
   vm_size             = var.vm_size
-  deployment_configuration {
-    virtual_machine_configuration {
-      image_reference {
-        publisher = var.image_publisher
-        offer     = var.image_offer
-        sku       = var.image_sku
-        version   = var.image_version
-      }
-      node_agent_sku_id = var.node_agent_sku_id
+  node_agent_sku_id   = var.node_agent_sku_id
+
+  virtual_machine_configuration {
+    image_reference {
+      publisher = var.image_publisher
+      offer     = var.image_offer
+      sku       = var.image_sku
+      version   = var.image_version
     }
+    node_agent_sku_id = var.node_agent_sku_id
   }
+
   scale_settings {
     fixed_scale {
       target_dedicated_nodes    = var.target_dedicated_nodes
       target_low_priority_nodes = var.target_low_priority_nodes
     }
   }
+
   network_configuration {
     subnet_id = var.subnet_id
   }
+
   start_task {
     command_line     = var.start_task_command_line
     wait_for_success = true
