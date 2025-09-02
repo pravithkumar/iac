cb99620a99d2b2f8ff0b8f17f5ece8a67eca5578
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-02 22:10:57 +0530

    Update variables.tf

--- optum-azurebatch/modules/network/variables.tf
+++ optum-azurebatch/modules/network/variables.tf
@@ -3,4 +3,7 @@ variable "resource_group_name" {}
 variable "vnet_name" {}
 variable "address_space" {}
 variable "subnet_name" {}
-variable "subnet_prefix" {}
+variable "subnet_prefix" {
+  type = list(string)
+  default = ["10.0.1.0/24"]
+}
