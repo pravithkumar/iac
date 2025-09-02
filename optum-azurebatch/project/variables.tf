1a1d9bb3282ae58c8b13cc730a0660b54afb5e62
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-02 21:28:13 +0530

    Update variables.tf

--- optum-azurebatch/project/variables.tf
+++ optum-azurebatch/project/variables.tf
@@ -1,32 +1,72 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "vnet_name" {}
-variable "vnet_address_space" {
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
   type = list(string)
 }
-variable "subnets" {
-  type = list(object({
-    name             = string
-    address_prefixes = list(string)
-    nsg_id           = optional(string)
-    route_table_id   = optional(string)
-    delegations = optional(list(object({
-      name          = string
-      service_name  = string
-      actions       = list(string)
-    })))
-  }))
+
+variable "subnet_name" {
+  type = string
 }
-variable "storage_account_name" {}
-variable "key_vault_name" {}
-variable "batch_account_name" {}
-variable "pool_name" {}
-variable "vm_size" {}
-variable "image_publisher" {}
-variable "image_offer" {}
-variable "image_sku" {}
-variable "image_version" {}
-variable "node_agent_sku_id" {}
-variable "target_dedicated_nodes" {}
-variable "target_low_priority_nodes" {}
-variable "start_task_command_line" {}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "key_vault_name" {
+  type = string
+}
+
+variable "batch_account_name" {
+  type = string
+}
+
+variable "pool_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "image_publisher" {
+  type = string
+}
+
+variable "image_offer" {
+  type = string
+}
+
+variable "image_sku" {
+  type = string
+}
+
+variable "image_version" {
+  type = string
+}
+
+variable "node_agent_sku_id" {
+  type = string
+}
+
+variable "target_dedicated_nodes" {
+  type = number
+}
+
+variable "target_low_priority_nodes" {
+  type = number
+}
+
+variable "start_task_command_line" {
+  type = string
+}
+
