01de63ad9bd43d000b1a1c8ba1c112c54e84aa01
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-02 22:48:47 +0530

    Update outputs.tf

--- optum-azurebatch/modules/keyvault/outputs.tf
+++ optum-azurebatch/modules/keyvault/outputs.tf
@@ -1,6 +1,9 @@
-output "key_vault_uri" {
-  value = azurerm_key_vault.akv.vault_uri
+output "id" {
+  description = "The ID of the Key Vault"
+  value       = azurerm_key_vault.keyvault.id
 }
-output "key_vault_id" {
-  value = azurerm_key_vault.akv.id
+
+output "vault_uri" {
+  description = "The URI of the Key Vault"
+  value       = azurerm_key_vault.keyvault.vault_uri
 }
