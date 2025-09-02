0254d6aa4a118abf110dedf39ebc76474304bfe3
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-02 20:54:49 +0530

    Update variables.tf

--- optum-azurebatch/project/variables.tf
+++ optum-azurebatch/project/variables.tf
@@ -10,10 +10,23 @@ variable "subnets" {
     address_prefixes = list(string)
     nsg_id           = optional(string)
     route_table_id   = optional(string)
-        delegations = optional(list(object({
+    delegations = optional(list(object({
       name          = string
       service_name  = string
       actions       = list(string)
     })))
   }))
 }
+variable "storage_account_name" {}
+variable "key_vault_name" {}
+variable "batch_account_name" {}
+variable "pool_name" {}
+variable "vm_size" {}
+variable "image_publisher" {}
+variable "image_offer" {}
+variable "image_sku" {}
+variable "image_version" {}
+variable "node_agent_sku_id" {}
+variable "target_dedicated_nodes" {}
+variable "target_low_priority_nodes" {}
+variable "start_task_command_line" {}
