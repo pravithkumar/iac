33ba7b38658faa0f730111e95f117abb669f0dff
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 16:34:50 +0530

    Update main.tf

--- azurebatch/modules/azurebatch/main.tf
+++ azurebatch/modules/azurebatch/main.tf
@@ -1,9 +1,11 @@
+
 resource "azurerm_batch_account" "batch" {
   name                          = var.batch_account_name
   location                      = var.location
   resource_group_name           = var.resource_group_name
   pool_allocation_mode          = "UserSubscription"
   public_network_access_enabled = false
+
   identity {
     type         = "UserAssigned"
     identity_ids = ["/subscriptions/795783af-96d3-4629-9161-58de5577ed1e/resourceGroups/optum-delete/providers/Microsoft.ManagedIdentity/userAssignedIdentities/optum-identity"]
@@ -19,14 +21,14 @@ resource "azurerm_batch_pool" "pool" {
   name                = var.pool_name
   resource_group_name = var.resource_group_name
   account_name        = azurerm_batch_account.batch.name
-  vm_size             = var.vm_size
-  node_agent_sku_id   = var.node_agent_sku_id
+  vm_size             = "Standard_D2_v3"  
+  node_agent_sku_id   = "batch.node.ubuntu 20.04" 
 
   storage_image_reference {
-    publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
+    publisher = "Canonical"             
+    offer     = "UbuntuServer"       
+    sku       = "20_04-lts"           
+    version   = "latest"           
   }
 
   fixed_scale {
@@ -39,7 +41,7 @@ resource "azurerm_batch_pool" "pool" {
   }
 
   start_task {
-    command_line     = var.start_task_command_line
+    command_line     = "/bin/bash -c 'echo Hello from Batch Pool'" 
     wait_for_success = true
 
     user_identity {
