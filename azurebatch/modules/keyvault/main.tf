0eca52ace202948ff92f6d7634791414f5dd5aa4
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 10:45:26 +0530

    Update main.tf

--- azurebatch/modules/keyvault/main.tf
+++ azurebatch/modules/keyvault/main.tf
@@ -6,6 +6,6 @@ resource "azurerm_key_vault" "akv" {
   resource_group_name         = var.resource_group_name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
+  #soft_delete_retention_days  = 7
+  #purge_protection_enabled    = true
 }
