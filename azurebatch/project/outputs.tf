ada2db6285c91ece1568ef25cd385f91c37d760d
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 16:13:59 +0530

    Update outputs.tf

--- azurebatch/project/outputs.tf
+++ azurebatch/project/outputs.tf
@@ -10,10 +10,6 @@ output "storage_account_id" {
   value = module.storage.storage_account_id
 }
 
-output "key_vault_url" {
-  value = module.keyvault.vault_url
-}
-
 output "subnet_id" {
   value = module.network.subnet_id
 }
