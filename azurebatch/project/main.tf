2be6c09badcca652923708146a39bc4e50dc367e
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 14:03:55 +0530

    Update main.tf

--- azurebatch/project/main.tf
+++ azurebatch/project/main.tf
@@ -56,4 +56,5 @@ module "azurebatch" {
   start_task_command_line   = var.start_task_command_line
   key_vault_id               = var.key_vault_id
   key_vault_url              = var.key_vault_url
+  user_assigned_identity_id  = data.azurerm_user_assigned_identity.batch_identity.principal_id
 }
