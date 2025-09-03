db5e54150ea0dc7d9f5748d6069a858c7a5d6feb
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 16:07:41 +0530

    Update main.tf

--- azurebatch/project/main.tf
+++ azurebatch/project/main.tf
@@ -18,18 +18,10 @@ module "storage" {
   storage_account_name = var.storage_account_name
 }
 
-# module "keyvault" {
-#   providers            =  {azurerm = azurerm.integ-nprod-001}
-#   source              = "../modules/keyvault"
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   key_vault_name      = var.key_vault_name
-# }
 
 module "access" {
   source = "../modules/access"
   providers = { azurerm = azurerm.integ-nprod-001 }
-
   principal_id        = "a3480609-66ce-4a3d-a25d-ae350e5b1bda"  
   storage_account_id  = module.storage.storage_account_id
   resource_group_id   = data.azurerm_resource_group.rg.id
@@ -52,8 +44,6 @@ module "azurebatch" {
   target_dedicated_nodes    = var.target_dedicated_nodes
   target_low_priority_nodes = var.target_low_priority_nodes
   subnet_id                 = module.network.subnet_id
-  start_task_command_line   = var.start_task_command_line
-  key_vault_id               = var.key_vault_id
-  key_vault_url              = var.key_vault_url
+  start_task_command_line   = var.start_task_command_line 
   user_assigned_identity_id  = "/subscriptions/795783af-96d3-4629-9161-58de5577ed1e/resourceGroups/optum-delete/providers/Microsoft.ManagedIdentity/userAssignedIdentities/optum-identity"
 }
