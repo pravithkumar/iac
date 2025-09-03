3cfe199ff228b27aa984307d3e31f513b72c758a
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-03 12:25:41 +0530

    Update data.tf

--- azurebatch/project/data.tf
+++ azurebatch/project/data.tf
@@ -1,3 +1,8 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+data "azurerm_user_assigned_identity" "batch_identity" {
+  name                = var.batch_identity_name
+  resource_group_name = var.resource_group_name
+}
+
