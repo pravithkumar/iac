d5a505504bb340701ef014fb21c93a2aeebc1aa2
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-04 13:06:53 +0530

    Update outputs.tf

--- azurebatch-2/project/outputs.tf
+++ azurebatch-2/project/outputs.tf
@@ -5,11 +5,3 @@ output "batch_account_id" {
 output "batch_pool_id" {
   value = module.azurebatch.batch_pool_id
 }
-
-output "storage_account_id" {
-  value = module.storage.storage_account_id
-}
-
-output "subnet_id" {
-  value = module.network.subnet_id
-}
