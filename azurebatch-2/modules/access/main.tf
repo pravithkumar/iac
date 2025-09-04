2af1f06af84c3c59d57e06a7dc9407feb7c0ded9
pravithkumar <84720606+pravithkumar@users.noreply.github.com>
2025-09-04 13:38:36 +0530

    Update main.tf

--- azurebatch-2/modules/access/main.tf
+++ azurebatch-2/modules/access/main.tf
@@ -1,11 +1,26 @@
-resource "azurerm_role_assignment" "umi_kv_access" {
-  scope                = var.key_vault_id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = var.user_assigned_identity_object_id
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault_access_policy" "umi_policy" {
+  key_vault_id = var.key_vault_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = var.user_assigned_identity_object_id
+
+  key_permissions = [
+    "get",
+    "wrapKey",
+    "unwrapKey"
+  ]
 }
 
-resource "azurerm_role_assignment" "batch_spn_kv_access" {
-  scope                = var.key_vault_id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "06d82036-5a43-4d4a-8203-a07095cb42bb"
+resource "azurerm_key_vault_access_policy" "batch_spn_policy" {
+  key_vault_id = var.key_vault_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "06d82036-5a43-4d4a-8203-a07095cb42bb"
+
+  key_permissions = [
+    "get",
+    "wrapKey",
+    "unwrapKey"
+  ]
 }
+
